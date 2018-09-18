7e0b296953c65de34244ec9183e2b874080d7367
Tim Vaillancourt <tim@timvaillancourt.com>
2018-09-18 16:00:47 +0200

    INITIAL COMMIT

---
+++ cmd/percona-server-mongodb-operator/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"runtime"
+	"time"
+
+	stub "github.com/timvaillancourt/percona-server-mongodb-operator/pkg/stub"
+	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
+	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
+	sdkVersion "github.com/operator-framework/operator-sdk/version"
+
+	"github.com/sirupsen/logrus"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+)
+
+func printVersion() {
+	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+}
+
+func main() {
+	printVersion()
+
+	sdk.ExposeMetricsPort()
+
+	resource := "cache.example.com/v1alpha1"
+	kind := "PerconaServerMongoDB"
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		logrus.Fatalf("failed to get watch namespace: %v", err)
+	}
+	resyncPeriod := time.Duration(5) * time.Second
+	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
+	sdk.Watch(resource, kind, namespace, resyncPeriod)
+	sdk.Handle(stub.NewHandler())
+	sdk.Run(context.TODO())
+}
---
+++ pkg/apis/cache/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type PerconaServerMongoDBList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []PerconaServerMongoDB `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type PerconaServerMongoDB struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              PerconaServerMongoDBSpec   `json:"spec"`
+	Status            PerconaServerMongoDBStatus `json:"status,omitempty"`
+}
+
+type PerconaServerMongoDBSpec struct {
+	// Fill me
+}
+type PerconaServerMongoDBStatus struct {
+	// Fill me
+}
