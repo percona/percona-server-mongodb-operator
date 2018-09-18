c904cd51ec6dadb1a25f1f91e0eaea3d3adffb66
Tim Vaillancourt <tim@timvaillancourt.com>
2018-09-18 22:05:59 +0200

    Make 3 x containers

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -2,6 +2,7 @@ package stub
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/timvaillancourt/percona-server-mongodb-operator/pkg/apis/cache/v1alpha1"
 
@@ -12,6 +13,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+var (
+	runUser  int64 = 1001
+	runGroup int64 = 1001
 )
 
 func NewHandler() sdk.Handler {
@@ -34,11 +40,67 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	return nil
 }
 
+func newPSMDBContainer(name string, port int32) corev1.Container {
+	portStr := strconv.Itoa(int(port))
+	return corev1.Container{
+		Name:    name,
+		Image:   "percona/percona-server-mongodb:3.6",
+		Command: []string{"--port=" + portStr},
+		Ports: []corev1.ContainerPort{
+			{
+				Name:          "mongodb",
+				HostPort:      port,
+				ContainerPort: port,
+				Protocol:      corev1.ProtocolTCP,
+			},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "mongodb-data",
+				MountPath: "/data/db",
+			},
+		},
+		WorkingDir: "/data/db",
+		ReadinessProbe: &corev1.Probe{
+			Handler: corev1.Handler{
+				TCPSocket: &corev1.TCPSocketAction{
+					Port: intstr.FromInt(int(port)),
+				},
+			},
+			InitialDelaySeconds: int32(60),
+			TimeoutSeconds:      int32(5),
+			PeriodSeconds:       int32(3),
+			FailureThreshold:    int32(5),
+		},
+		SecurityContext: &corev1.SecurityContext{
+			RunAsUser:  &runUser,
+			RunAsGroup: &runGroup,
+		},
+		Env: []corev1.EnvVar{
+			{
+				Name:  "MONGODB_PORT",
+				Value: portStr,
+			},
+		},
+	}
+}
+
 // newPSMDBPod
 func newPSMDBPod(cr *v1alpha1.PerconaServerMongoDB) *corev1.Pod {
 	labels := map[string]string{
 		"app": "percona-server-mongodb",
 	}
+
+	containers := []corev1.Container{}
+	mongods := map[string]int32{
+		"percona-server-mongodb-0": 27017,
+		"percona-server-mongodb-1": 27018,
+		"percona-server-mongodb-2": 27019,
+	}
+	for name, port := range mongods {
+		containers = append(containers, newPSMDBContainer(name, port))
+	}
+
 	return &corev1.Pod{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Pod",
@@ -57,34 +119,7 @@ func newPSMDBPod(cr *v1alpha1.PerconaServerMongoDB) *corev1.Pod {
 			Labels: labels,
 		},
 		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "percona-server-mongodb",
-					Image:   "percona/percona-server-mongodb:3.6",
-					Command: []string{"--port=27017"},
-					Ports: []corev1.ContainerPort{{
-						Name:          "mongodb",
-						HostPort:      int32(27017),
-						ContainerPort: int32(27017),
-						Protocol:      corev1.ProtocolTCP,
-					}},
-					VolumeMounts: []corev1.VolumeMount{{
-						Name:      "mongodb-data",
-						MountPath: "/data/db",
-					}},
-					ReadinessProbe: &corev1.Probe{
-						Handler: corev1.Handler{
-							TCPSocket: &corev1.TCPSocketAction{
-								Port: intstr.FromInt(27017),
-							},
-						},
-						InitialDelaySeconds: int32(60),
-						TimeoutSeconds:      int32(5),
-						PeriodSeconds:       int32(3),
-						FailureThreshold:    int32(5),
-					},
-				},
-			},
+			Containers: containers,
 		},
 	}
 }
