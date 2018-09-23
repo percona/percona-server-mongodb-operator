a95f926aed6f67d63a6c1b36057ee88a2a9ea742
Tim Vaillancourt <tim@timvaillancourt.com>
2018-09-23 17:24:33 +0200

    Fix main version

--- cmd/percona-server-mongodb-operator/main.go
+++ cmd/percona-server-mongodb-operator/main.go
@@ -9,20 +9,17 @@ import (
 	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	stub "github.com/timvaillancourt/percona-server-mongodb-operator/pkg/stub"
+	version "github.com/timvaillancourt/percona-server-mongodb-operator/version"
 
 	"github.com/sirupsen/logrus"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-)
-
-var (
-	Version = "0.0.1"
 )
 
 func printVersion() {
 	logrus.Infof("Go Version: %s", runtime.Version())
 	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
 	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
-	logrus.Infof("percona-server-mongodb-operator version: %v", Version)
+	logrus.Infof("percona-server-mongodb-operator version: %v", version.Version)
 }
 
 func main() {
