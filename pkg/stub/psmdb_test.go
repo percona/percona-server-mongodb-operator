0a503a7d48a558ecfaf89e78bbd4af3c78b33fc7
Tim Vaillancourt <tim@timvaillancourt.com>
2018-10-23 19:39:27 +0200

    Fix psmdb_test.go

--- pkg/stub/psmdb_test.go
+++ pkg/stub/psmdb_test.go
@@ -14,20 +14,20 @@ func TestGetWiredTigerCacheSizeGB(t *testing.T) {
 	memoryQuantity := resource.NewQuantity(gigaByte/2, resource.DecimalSI)
 	assert.Equal(t, 0.25, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
 
-	//	memoryQuantity = resource.NewQuantity(1*gigaByte, resource.DecimalSI)
-	//	assert.Equal(t, 0.25, getWiredTigerCacheSizeGB(memoryQuantity, 0.5))
-	//
-	//	memoryQuantity = resource.NewQuantity(4*gigaByte, resource.DecimalSI)
-	//	assert.Equal(t, 1.5, getWiredTigerCacheSizeGB(memoryQuantity, 0.5))
-	//
-	//	memoryQuantity = resource.NewQuantity(64*gigaByte, resource.DecimalSI)
-	//	assert.Equal(t, 31.5, getWiredTigerCacheSizeGB(memoryQuantity, 0.5))
-	//
-	//	memoryQuantity = resource.NewQuantity(128*gigaByte, resource.DecimalSI)
-	//	assert.Equal(t, 63.5, getWiredTigerCacheSizeGB(memoryQuantity, 0.5))
-	//
-	//	memoryQuantity = resource.NewQuantity(256*gigaByte, resource.DecimalSI)
-	//	assert.Equal(t, 127.5, getWiredTigerCacheSizeGB(memoryQuantity, 0.5))
+	memoryQuantity = resource.NewQuantity(1*gigaByte, resource.DecimalSI)
+	assert.Equal(t, 0.25, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
+
+	memoryQuantity = resource.NewQuantity(4*gigaByte, resource.DecimalSI)
+	assert.Equal(t, 1.5, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
+
+	memoryQuantity = resource.NewQuantity(64*gigaByte, resource.DecimalSI)
+	assert.Equal(t, 31.5, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
+
+	memoryQuantity = resource.NewQuantity(128*gigaByte, resource.DecimalSI)
+	assert.Equal(t, 63.5, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
+
+	memoryQuantity = resource.NewQuantity(256*gigaByte, resource.DecimalSI)
+	assert.Equal(t, 127.5, getWiredTigerCacheSizeGB(corev1.ResourceList{corev1.ResourceMemory: *memoryQuantity}, 0.5))
 }
 
 func TestAddPSMDBSpecDefaults(t *testing.T) {
@@ -35,9 +35,9 @@ func TestAddPSMDBSpecDefaults(t *testing.T) {
 
 	addPSMDBSpecDefaults(&spec)
 
-	assert.Equal(t, defaultSize, spec.Size)
 	assert.Equal(t, defaultImage, spec.Image)
 	assert.NotNil(t, spec.Mongod)
+	assert.Equal(t, defaultMongodSize, spec.Mongod.Size)
 	assert.Equal(t, defaultStorageEngine, spec.Mongod.StorageEngine)
 	assert.NotNil(t, spec.Mongod.WiredTiger)
 	assert.Equal(t, defaultWiredTigerCacheSizeRatio, spec.Mongod.WiredTiger.CacheSizeRatio)
