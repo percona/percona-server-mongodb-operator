3f9bfea30aed0307f84cfc4a97775330b77295e5
Tim Vaillancourt <tim@timvaillancourt.com>
2018-10-24 23:36:27 +0200

    Add SDK mock interface

---
+++ pkg/sdk/client.go
@@ -0,0 +1,45 @@
+package sdk
+
+import (
+	opSdk "github.com/operator-framework/operator-sdk/pkg/sdk"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type Client interface {
+	Create(object opSdk.Object) error
+	Patch(object opSdk.Object, pt types.PatchType, patch []byte) error
+	Update(object opSdk.Object) error
+	Delete(object opSdk.Object, opts ...opSdk.DeleteOption) error
+	Get(into opSdk.Object, opts ...opSdk.GetOption) error
+	List(namespace string, into opSdk.Object, opts ...opSdk.ListOption)
+}
+
+type SDKClient struct{}
+
+func NewClient() *SDKClient {
+	return &SDKClient{}
+}
+
+func (c *SDKClient) Create(object opSdk.Object) error {
+	return opSdk.Create(object)
+}
+
+func (c *SDKClient) Patch(object opSdk.Object, pt types.PatchType, patch []byte) error {
+	return opSdk.Patch(object, pt, patch)
+}
+
+func (c *SDKClient) Update(object opSdk.Object) error {
+	return opSdk.Update(object)
+}
+
+func (c *SDKClient) Delete(object opSdk.Object, opts ...opSdk.DeleteOption) error {
+	return opSdk.Delete(object, opts...)
+}
+
+func (c *SDKClient) Get(into opSdk.Object, opts ...opSdk.GetOption) error {
+	return opSdk.Get(into, opts...)
+}
+
+func (c *SDKClient) List(namespace string, into opSdk.Object, opts ...opSdk.ListOption) error {
+	return opSdk.List(namespace, into, opts...)
+}
