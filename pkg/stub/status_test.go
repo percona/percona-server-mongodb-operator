3ecf3abc6db36ecc58407967ed4685ec3dfc0a7a
Tim Vaillancourt <tim@timvaillancourt.com>
2018-11-22 20:34:46 +0100

    Add a test

--- pkg/stub/status_test.go
+++ pkg/stub/status_test.go
@@ -1,10 +1,30 @@
 package stub
 
 import (
-//	"testing"
+	"testing"
 
-//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
 )
+
+func TestIsStatefulSetUpdating(t *testing.T) {
+	ss := &appsv1.StatefulSet{
+		Status: appsv1.StatefulSetStatus{
+			CurrentReplicas: defaultMongodSize,
+			ReadyReplicas:   defaultMongodSize,
+			CurrentRevision: t.Name(),
+			UpdateRevision:  t.Name(),
+		},
+	}
+	assert.False(t, isStatefulSetUpdating(ss))
+
+	ss.Status.UpdateRevision = ss.Status.UpdateRevision + "-true"
+	assert.True(t, isStatefulSetUpdating(ss))
+	ss.Status.UpdateRevision = t.Name()
+
+	ss.Status.ReadyReplicas -= 1
+	assert.True(t, isStatefulSetUpdating(ss))
+}
 
 //func TestGetReplsetStatus(t *testing.T) {}
 //func TestGetReplsetMemberStatuses(t *testing.T) {}
