e48912d15acbe38e1569af86cf5f60362b808dd6
Sergey <heartwilltell@gmail.com>
2018-12-27 17:32:34 +0200

    fix panic in testa

--- pkg/stub/psmdb.go
+++ pkg/stub/psmdb.go
@@ -46,6 +46,9 @@ func (h *Handler) addSpecDefaults(m *v1alpha1.PerconaServerMongoDB) {
 	if spec.Mongod.Storage.Engine == "" {
 		spec.Mongod.Storage.Engine = config.DefaultStorageEngine
 	}
+	if spec.Expose == nil {
+		spec.Expose = &v1alpha1.Expose{}
+	}
 	if spec.Expose.Enabled && spec.Expose.ExposeType == "" {
 		spec.Expose.ExposeType = corev1.ServiceTypeClusterIP
 	}
