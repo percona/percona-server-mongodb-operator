9bc8cb14096922a88a8cd0c09d6ae75e08b6fe30
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-01-09 17:34:14 +0200

    Moved types

--- pkg/apis/psmdb/v1alpha1/perconaservermongodb_types.go
+++ pkg/apis/psmdb/v1alpha1/perconaservermongodb_types.go
@@ -1,23 +1,10 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sversion "k8s.io/apimachinery/pkg/version"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// PerconaServerMongoDBSpec defines the desired state of PerconaServerMongoDB
-type PerconaServerMongoDBSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-}
-
-// PerconaServerMongoDBStatus defines the observed state of PerconaServerMongoDB
-type PerconaServerMongoDBStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -38,6 +25,222 @@ type PerconaServerMongoDBList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []PerconaServerMongoDB `json:"items"`
+}
+
+type Platform string
+
+const (
+	PlatformKubernetes Platform = "kubernetes"
+	PlatformOpenshift  Platform = "openshift"
+)
+
+type ClusterRole string
+
+const (
+	ClusterRoleShardSvr  ClusterRole = "shardsvr"
+	ClusterRoleConfigSvr ClusterRole = "configsvr"
+)
+
+// PerconaServerMongoDBSpec defines the desired state of PerconaServerMongoDB
+type PerconaServerMongoDBSpec struct {
+	Platform        *Platform         `json:"platform,omitempty"`
+	Version         string            `json:"version,omitempty"`
+	RunUID          int64             `json:"runUid,omitempty"`
+	Mongod          *MongodSpec       `json:"mongod,omitempty"`
+	Replsets        []*ReplsetSpec    `json:"replsets,omitempty"`
+	Secrets         *SecretsSpec      `json:"secrets,omitempty"`
+	Backup          *BackupSpec       `json:"backup,omitempty"`
+	ImagePullPolicy corev1.PullPolicy `json:"imagePullPolicy,omitempty"`
+	Expose          *Expose           `json:"expose,omitempty"`
+}
+
+// PerconaServerMongoDBStatus defines the observed state of PerconaServerMongoDB
+type PerconaServerMongoDBStatus struct {
+	Replsets []*ReplsetStatus `json:"replsets,omitempty"`
+}
+
+type ReplsetSpec struct {
+	*ResourcesSpec `json:"resources,omitempty"`
+	Name           string      `json:"name"`
+	Size           int32       `json:"size"`
+	StorageClass   string      `json:"storageClass,omitempty"`
+	ClusterRole    ClusterRole `json:"clusterRole,omitempty"`
+}
+
+type ResourceSpecRequirements struct {
+	Cpu     string `json:"cpu,omitempty"`
+	Memory  string `json:"memory,omitempty"`
+	Storage string `json:"storage,omitempty"`
+}
+
+type ResourcesSpec struct {
+	Limits       *ResourceSpecRequirements `json:"limits,omitempty"`
+	Requests     *ResourceSpecRequirements `json:"requests,omitempty"`
+	StorageClass string                    `json:"storageClass,omitempty"`
+}
+
+// ServerVersion represents info about k8s / openshift server version
+type ServerVersion struct {
+	Platform Platform
+	Info     k8sversion.Info
+}
+
+type SecretsSpec struct {
+	Key   string `json:"key,omitempty"`
+	Users string `json:"users,omitempty"`
+}
+
+type ReplsetMemberStatus struct {
+	Name    string `json:"name,omitempty"`
+	Version string `json:"version,omitempty"`
+}
+
+type ReplsetStatus struct {
+	Name        string                 `json:"name,omitempty"`
+	Pods        []string               `json:"pods,omitempty"`
+	Members     []*ReplsetMemberStatus `json:"members,omitempty"`
+	ClusterRole ClusterRole            `json:"clusterRole,omitempty"`
+	Initialized bool                   `json:"initialized,omitempty"`
+}
+
+type MongosSpec struct {
+	*ResourcesSpec `json:"resources,omitempty"`
+	Port           int32 `json:"port,omitempty"`
+	HostPort       int32 `json:"hostPort,omitempty"`
+}
+
+type MongodSpec struct {
+	Net                *MongodSpecNet                `json:"net,omitempty"`
+	AuditLog           *MongodSpecAuditLog           `json:"auditLog,omitempty"`
+	OperationProfiling *MongodSpecOperationProfiling `json:"operationProfiling,omitempty"`
+	Replication        *MongodSpecReplication        `json:"replication,omitempty"`
+	Security           *MongodSpecSecurity           `json:"security,omitempty"`
+	SetParameter       *MongodSpecSetParameter       `json:"setParameter,omitempty"`
+	Storage            *MongodSpecStorage            `json:"storage,omitempty"`
+}
+
+type MongodSpecNet struct {
+	Port     int32 `json:"port,omitempty"`
+	HostPort int32 `json:"hostPort,omitempty"`
+}
+
+type MongodSpecReplication struct {
+	OplogSizeMB int `json:"oplogSizeMB,omitempty"`
+}
+
+type MongodSpecSecurity struct {
+	RedactClientLogData bool `json:"redactClientLogData,omitempty"`
+}
+
+type MongodSpecSetParameter struct {
+	TTLMonitorSleepSecs                   int `json:"ttlMonitorSleepSecs,omitempty"`
+	WiredTigerConcurrentReadTransactions  int `json:"wiredTigerConcurrentReadTransactions,omitempty"`
+	WiredTigerConcurrentWriteTransactions int `json:"wiredTigerConcurrentWriteTransactions,omitempty"`
+}
+
+type StorageEngine string
+
+var (
+	StorageEngineWiredTiger StorageEngine = "wiredTiger"
+	StorageEngineInMemory   StorageEngine = "inMemory"
+	StorageEngineMMAPv1     StorageEngine = "mmapv1"
+)
+
+type MongodSpecStorage struct {
+	Engine         StorageEngine         `json:"engine,omitempty"`
+	DirectoryPerDB bool                  `json:"directoryPerDB,omitempty"`
+	SyncPeriodSecs int                   `json:"syncPeriodSecs,omitempty"`
+	InMemory       *MongodSpecInMemory   `json:"inMemory,omitempty"`
+	MMAPv1         *MongodSpecMMAPv1     `json:"mmapv1,omitempty"`
+	WiredTiger     *MongodSpecWiredTiger `json:"wiredTiger,omitempty"`
+}
+
+type MongodSpecMMAPv1 struct {
+	NsSize     int  `json:"nsSize,omitempty"`
+	Smallfiles bool `json:"smallfiles,omitempty"`
+}
+
+type WiredTigerCompressor string
+
+var (
+	WiredTigerCompressorNone   WiredTigerCompressor = "none"
+	WiredTigerCompressorSnappy WiredTigerCompressor = "snappy"
+	WiredTigerCompressorZlib   WiredTigerCompressor = "zlib"
+)
+
+type MongodSpecWiredTigerEngineConfig struct {
+	CacheSizeRatio      float64               `json:"cacheSizeRatio,omitempty"`
+	DirectoryForIndexes bool                  `json:"directoryForIndexes,omitempty"`
+	JournalCompressor   *WiredTigerCompressor `json:"journalCompressor,omitempty"`
+}
+
+type MongodSpecWiredTigerCollectionConfig struct {
+	BlockCompressor *WiredTigerCompressor `json:"blockCompressor,omitempty"`
+}
+
+type MongodSpecWiredTigerIndexConfig struct {
+	PrefixCompression bool `json:"prefixCompression,omitempty"`
+}
+
+type MongodSpecWiredTiger struct {
+	CollectionConfig *MongodSpecWiredTigerCollectionConfig `json:"collectionConfig,omitempty"`
+	EngineConfig     *MongodSpecWiredTigerEngineConfig     `json:"engineConfig,omitempty"`
+	IndexConfig      *MongodSpecWiredTigerIndexConfig      `json:"indexConfig,omitempty"`
+}
+
+type MongodSpecInMemoryEngineConfig struct {
+	InMemorySizeRatio float64 `json:"inMemorySizeRatio,omitempty"`
+}
+
+type MongodSpecInMemory struct {
+	EngineConfig *MongodSpecInMemoryEngineConfig `json:"engineConfig,omitempty"`
+}
+
+type AuditLogDestination string
+
+var AuditLogDestinationFile AuditLogDestination = "file"
+
+type AuditLogFormat string
+
+var (
+	AuditLogFormatBSON AuditLogFormat = "BSON"
+	AuditLogFormatJSON AuditLogFormat = "JSON"
+)
+
+type MongodSpecAuditLog struct {
+	Destination AuditLogDestination `json:"destination,omitempty"`
+	Format      AuditLogFormat      `json:"format,omitempty"`
+	Filter      string              `json:"filter,omitempty"`
+}
+
+type OperationProfilingMode string
+
+const (
+	OperationProfilingModeAll    OperationProfilingMode = "all"
+	OperationProfilingModeSlowOp OperationProfilingMode = "slowOp"
+)
+
+type MongodSpecOperationProfiling struct {
+	Mode              OperationProfilingMode `json:"mode,omitempty"`
+	SlowOpThresholdMs int                    `json:"slowOpThresholdMs,omitempty"`
+	RateLimit         int                    `json:"rateLimit,omitempty"`
+}
+
+type BackupCoordinatorSpec struct {
+	*ResourcesSpec `json:"resources,omitempty"`
+	Debug          bool `json:"debug,omitempty"`
+}
+
+type BackupSpec struct {
+	Enabled          bool                   `json:"enabled"`
+	Version          string                 `json:"version,omitempty"`
+	RestartOnFailure *bool                  `json:"restartOnFailure,omitempty"`
+	Coordinator      *BackupCoordinatorSpec `json:"coordinator,omitempty"`
+}
+
+type Expose struct {
+	Enabled    bool               `json:"enabled"`
+	ExposeType corev1.ServiceType `json:"exposeType,omitempty"`
 }
 
 func init() {
