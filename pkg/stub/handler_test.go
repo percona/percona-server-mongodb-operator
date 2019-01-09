4532b6ce6d1bce92647194e9f43464e42b5fba80
Sergey <heartwilltell@gmail.com>
2019-01-09 21:40:32 +0200

    generate deepcopy funcs

--- pkg/stub/handler_test.go
+++ pkg/stub/handler_test.go
@@ -82,7 +82,7 @@ func TestHandlerHandle(t *testing.T) {
 	client.On("Update", mock.AnythingOfType("*v1alpha1.PerconaServerMongoDB")).Return(nil)
 	client.On("Get", mock.AnythingOfType("*v1alpha1.PerconaServerMongoDB")).Return(nil)
 	client.On("Get", mock.AnythingOfType("*v1.Secret")).Return(nil)
-	client.On("Get", mock.AnythingOfType("*v1.StatefulSet")).Return(errors.New("not found error")).Once()
+	client.On("Get", mock.AnythingOfType("*v1.StatefulSet")).Return(errors.New("not found error"))
 	client.On("List",
 		"test",
 		mock.AnythingOfType("*v1.PodList"),
