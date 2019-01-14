bd26e1d5fdafb98fae82a60333b079fa450887a1
Tim Vaillancourt <tim@timvaillancourt.com>
2019-01-14 22:21:32 +0100

    Fix golang ci suggestion

--- pkg/stub/exec_test.go
+++ pkg/stub/exec_test.go
@@ -13,13 +13,13 @@ func TestPrintCommandOutput(t *testing.T) {
 	_, err := output.stdout.WriteString("test stdout")
 	assert.NoError(t, err)
 
-	printCommandOutput("test", t.Name(), output, &out)
+	assert.NoError(t, printCommandOutput("test", t.Name(), output, &out))
 	assert.Regexp(t, "test stdout\n$", out.String())
 	assert.NotRegexp(t, "stderr\n$", out.String())
 	out.Reset()
 
 	_, err = output.stderr.WriteString("test stderr")
 	assert.NoError(t, err)
-	printCommandOutput("test", t.Name(), output, &out)
+	assert.NoError(t, printCommandOutput("test", t.Name(), output, &out))
 	assert.Regexp(t, "test stderr\n$", out.String())
 }
