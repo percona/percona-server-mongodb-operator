65a9f5d88cac086ccf42b301b93ddc99f49b4bea
Tim Vaillancourt <tim@timvaillancourt.com>
2019-01-14 20:44:59 +0100

    CLOUD-87: Add timeout to execCommandInContainer()

--- pkg/stub/exec.go
+++ pkg/stub/exec.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/Percona-Lab/percona-server-mongodb-operator/internal/mongod"
 	"github.com/Percona-Lab/percona-server-mongodb-operator/internal/util"
@@ -17,6 +18,8 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/remotecommand"
 )
+
+var execCommandTimeout = 30 * time.Second
 
 // printOutput outputs stdout/stderr log buffers from commands
 func printOutputBuffer(cmd, pod string, r io.Reader, out io.Writer) error {
@@ -75,40 +78,53 @@ func execCommandInContainer(pod corev1.Pod, containerName string, cmd []string) 
 		return fmt.Errorf("cannot find mongod port in container: %s", container.Name)
 	}
 
-	req := client.CoreV1().RESTClient().Post().
-		Resource("pods").
-		Name(pod.Name).
-		Namespace(pod.Namespace).
-		SubResource("exec")
-	req.VersionedParams(&corev1.PodExecOptions{
-		Container: containerName,
-		Command:   cmd,
-		Stdout:    true,
-		Stderr:    true,
-	}, scheme.ParameterCodec)
-
-	exec, err := remotecommand.NewSPDYExecutor(cfg, "POST", req.URL())
-	if err != nil {
-		return fmt.Errorf("failed to run exec in pod %s: %v", pod.Name, err)
+	type Output struct {
+		stdout bytes.Buffer
+		stderr bytes.Buffer
 	}
+	outChan := make(chan Output)
 
-	logrus.WithFields(logrus.Fields{
-		"pod":       pod.Name,
-		"container": containerName,
-		"command":   cmd[0],
-	}).Info("running command in container")
+	go func() {
+		req := client.CoreV1().RESTClient().Post().
+			Resource("pods").
+			Name(pod.Name).
+			Namespace(pod.Namespace).
+			SubResource("exec")
+		req.VersionedParams(&corev1.PodExecOptions{
+			Container: containerName,
+			Command:   cmd,
+			Stdout:    true,
+			Stderr:    true,
+		}, scheme.ParameterCodec)
 
-	var (
-		stdOut bytes.Buffer
-		stdErr bytes.Buffer
-	)
-	err = exec.Stream(remotecommand.StreamOptions{
-		Stdout: &stdOut,
-		Stderr: &stdErr,
-	})
-	if err != nil {
-		logrus.Errorf("error running remote command %s: %v", cmd[0], err)
+		exec, err := remotecommand.NewSPDYExecutor(cfg, "POST", req.URL())
+		if err != nil {
+			logrus.Errorf("failed to run exec in pod %s: %v", pod.Name, err)
+			return
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"pod":       pod.Name,
+			"container": containerName,
+			"command":   cmd[0],
+		}).Info("running command in container")
+
+		out := Output{}
+		err = exec.Stream(remotecommand.StreamOptions{
+			Stdout: &out.stdout,
+			Stderr: &out.stderr,
+		})
+		if err != nil {
+			logrus.Errorf("error running remote command %s: %v", cmd[0], err)
+		}
+		outChan <- out
+	}()
+
+	select {
+	case <-time.After(execCommandTimeout):
+		logrus.Errorf("timeout reach executing command on pod %s: %s", pod.Name, cmd[0])
+		return fmt.Errorf("timeout executing command")
+	case out := <-outChan:
+		return printCommandOutput(cmd[0], pod.Name, &out.stdout, &out.stderr, os.Stdout)
 	}
-
-	return printCommandOutput(cmd[0], pod.Name, &stdOut, &stdErr, os.Stdout)
 }
