a82c8a1865135db5dbbe1898211dbe0c5866355a
Sergey <heartwilltell@gmail.com>
2019-02-09 12:51:39 +0200

    fix to setSafeDefault

--- pkg/stub/unsafeconf.go
+++ pkg/stub/unsafeconf.go
@@ -3,12 +3,15 @@ package stub
 import "github.com/Percona-Lab/percona-server-mongodb-operator/pkg/apis/psmdb/v1alpha1"
 
 func setSafeDefault(replset *v1alpha1.ReplsetSpec) {
-	if replset.Size > 3 {
-		replset.Size = 3
-	}
-
 	if replset.Arbiter != nil && replset.Arbiter.Enabled {
 		replset.Arbiter.Size = 1
-		replset.Size = 2
+
+		if (replset.Size+replset.Arbiter.Size)%2 == 0 {
+			replset.Size += 1
+		}
+	}
+
+	if replset.Size%2 == 0 {
+		replset.Size += 1
 	}
 }
