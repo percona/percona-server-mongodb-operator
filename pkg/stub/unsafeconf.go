277a2dabea93e698ae0b0f1397cdcb2344590523
Sergey <heartwilltell@gmail.com>
2019-02-11 13:26:11 +0200

    logs

--- pkg/stub/unsafeconf.go
+++ pkg/stub/unsafeconf.go
@@ -7,30 +7,27 @@ import (
 
 func setSafeDefault(replset *v1alpha1.ReplsetSpec) {
 	arbiterDefaults(replset)
-	var dolog bool
 
 	if replset.Arbiter != nil && replset.Arbiter.Enabled {
 
 		if replset.Arbiter.Size > 1 {
-			logrus.Infof("allowUnsafeConfigurations option has been disabled. Config will be optimized for safe usage!")
-			logrus.Infof("Arbiter size will be decreased from %d to 1", replset.Arbiter.Size)
+			logrus.Infof("Arbiter size will be decreased from %d to 1 due to safe config", replset.Arbiter.Size)
+			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
 			replset.Arbiter.Size = 1
-			dolog = true
 		}
 
 		if (replset.Size+replset.Arbiter.Size)%2 == 0 {
-			if dolog {
-				logrus.Infof("allowUnsafeConfigurations option has been disabled. Config will be optimized for safe usage!")
-			}
 			logrus.Infof("Replset size will be increased from %d to %d", replset.Size, replset.Size+1)
+			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
+
 			replset.Size++
 		}
 
 	} else {
 
 		if replset.Size%2 == 0 {
-			logrus.Infof("allowUnsafeConfigurations option has been disabled. Config will be optimized for safe usage!")
 			logrus.Infof("Replset size will be increased from %d to %d", replset.Size, replset.Size+1)
+			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
 			replset.Size++
 		}
 	}
