0f99ca1078c4483d76334948a42877069f0d2bf7
Sergey <heartwilltell@gmail.com>
2019-02-11 13:26:44 +0200

    add unsafe config functionality

--- pkg/stub/unsafeconf.go
+++ pkg/stub/unsafeconf.go
@@ -12,13 +12,13 @@ func setSafeDefault(replset *v1alpha1.ReplsetSpec) {
 
 		if replset.Arbiter.Size > 1 {
 			logrus.Infof("Arbiter size will be decreased from %d to 1 due to safe config", replset.Arbiter.Size)
-			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
+			logrus.Infof("Set allowUnsafeConfigurations=true to disable safe configuration")
 			replset.Arbiter.Size = 1
 		}
 
 		if (replset.Size+replset.Arbiter.Size)%2 == 0 {
 			logrus.Infof("Replset size will be increased from %d to %d", replset.Size, replset.Size+1)
-			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
+			logrus.Infof("Set allowUnsafeConfigurations=true to disable safe configuration")
 
 			replset.Size++
 		}
@@ -27,7 +27,7 @@ func setSafeDefault(replset *v1alpha1.ReplsetSpec) {
 
 		if replset.Size%2 == 0 {
 			logrus.Infof("Replset size will be increased from %d to %d", replset.Size, replset.Size+1)
-			logrus.Infof("set allowUnsafeConfigurations=true to disable safe configuration")
+			logrus.Infof("Set allowUnsafeConfigurations=true to disable safe configuration")
 			replset.Size++
 		}
 	}
