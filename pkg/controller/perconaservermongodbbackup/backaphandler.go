cc931be545f9d68cbbb1b20b162d1042f7984f41
Max Dudin <dudin.mv@gmail.com>
2019-04-30 11:36:40 +0300

    Clean up

--- pkg/controller/perconaservermongodbbackup/backaphandler.go
+++ pkg/controller/perconaservermongodbbackup/backaphandler.go
@@ -2,6 +2,7 @@ package perconaservermongodbbackup
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	pbapi "github.com/percona/percona-backup-mongodb/proto/api"
@@ -18,6 +19,7 @@ func newBackupHandler(crName, backupName, storageName string) (BackupHandler, er
 	if err != nil {
 		return b, err
 	}
+
 	client := pbapi.NewApiClient(conn)
 	bData := BackupData{
 		StorageName: storageName,
@@ -31,16 +33,17 @@ func newBackupHandler(crName, backupName, storageName string) (BackupHandler, er
 		Client:     client,
 		BackupData: bData,
 	}
+
 	return b, nil
 }
 
-// BackupHandler implements BC
+// BackupHandler is for working with backup coordinator
 type BackupHandler struct {
 	Client     pbapi.ApiClient
 	BackupData BackupData
 }
 
-// Back
+// BackupData is for storing backup data
 type BackupData struct {
 	Status      string
 	Start       int64
@@ -56,7 +59,6 @@ func (b *BackupHandler) BackupExist() (bool, BackupData, error) {
 	if err != nil {
 		return false, BackupData{}, err
 	}
-
 	for {
 		msg, err := stream.Recv()
 		if err != nil {
@@ -69,32 +71,66 @@ func (b *BackupHandler) BackupExist() (bool, BackupData, error) {
 			b.BackupData.Start = msg.Metadata.StartTs
 			if msg.Metadata.EndTs > 0 {
 				b.BackupData.Status = "ready"
-			} else {
-				b.BackupData.Status = "running"
 			}
 			return true, b.BackupData, nil
-
 		}
 	}
+
 	return false, BackupData{}, nil
 }
 
 // StartBackup is for starting new backup
 func (b *BackupHandler) StartBackup() error {
+
+	stream, err := b.Client.ListStorages(context.Background(), &pbapi.ListStoragesParams{})
+	if err != nil {
+		return fmt.Errorf("cannot list storages")
+	}
+
+	storages := []pbapi.StorageInfo{}
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		storages = append(storages, *msg)
+	}
+
+	// Checking is coordinator have availeble storage
+	in := false
+	for _, s := range storages {
+		if s.Name == b.BackupData.StorageName {
+			in = true
+		}
+	}
+	if !in {
+		return fmt.Errorf("storage not availeble")
+	}
+
 	msg := &pbapi.RunBackupParams{
-		CompressionType: pbapi.CompressionType_COMPRESSION_TYPE_NO_COMPRESSION,
+		CompressionType: pbapi.CompressionType_COMPRESSION_TYPE_GZIP,
 		Cypher:          pbapi.Cypher_CYPHER_NO_CYPHER,
 		Description:     b.BackupData.Name,
 		StorageName:     b.BackupData.StorageName,
 	}
-	_, err := b.Client.RunBackup(context.Background(), msg)
+	resp, err := b.Client.RunBackup(context.Background(), msg)
 	if err != nil {
-		b.BackupData.Status = "notrunning"
+		b.BackupData.Status = "rejected"
 		b.BackupData.End = 0
 		return err
 	}
-
-	b.BackupData.Status = "rejected"
+	if resp != nil {
+		if resp.Code > 0 {
+			log.Info("Backup resp code:", resp.Code)
+		}
+		if len(resp.Message) > 0 {
+			log.Info("Backup msg", resp.Message)
+		}
+	}
+	b.BackupData.Status = "running"
 	b.BackupData.End = 0
 
 	return nil
