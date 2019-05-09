41aa5d93a6032cf9d1b9489841b420300afad7c0
Patrick Birch <48594400+patrickbirch@users.noreply.github.com>
2019-05-09 14:27:25 -0500

    update conf.py
    fixed spelling

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ release = '0.4.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions=['sphinx.ext.intersphinx', 'spinx.ext.todo',
+extensions=['sphinx.ext.intersphinx', 'sphinx.ext.todo',
             'sphinx.ext.coverage', 'sphinx.ext.ifconfig','sphinx.ext.extlinks', 'sphinx-prompt', 'psdom', ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
