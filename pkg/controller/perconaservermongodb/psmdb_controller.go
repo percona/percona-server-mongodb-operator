dd60d4822d626fda109d05b8fb37df97f3dfbf01
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-05-14 07:49:24 +0300

    Use pmm secret keys only if set (#193)

--- pkg/controller/perconaservermongodb/psmdb_controller.go
+++ pkg/controller/perconaservermongodb/psmdb_controller.go
@@ -470,7 +470,19 @@ func (r *ReconcilePerconaServerMongoDB) reconcileStatefulSet(arbiter bool, cr *a
 		}
 
 		if cr.Spec.PMM.Enabled {
-			sfsSpec.Template.Spec.Containers = append(sfsSpec.Template.Spec.Containers, psmdb.PMMContainer(cr.Spec.PMM, cr.Spec.Secrets.Users))
+			pmmsec := corev1.Secret{}
+			err := r.client.Get(context.TODO(), types.NamespacedName{Name: cr.Spec.Secrets.Users, Namespace: cr.Namespace}, &pmmsec)
+			if err != nil {
+				return nil, fmt.Errorf("check pmm secrets: %v", err)
+			}
+
+			_, okl := pmmsec.Data[psmdb.PMMUserKey]
+			_, okp := pmmsec.Data[psmdb.PMMPasswordKey]
+
+			sfsSpec.Template.Spec.Containers = append(
+				sfsSpec.Template.Spec.Containers,
+				psmdb.PMMContainer(cr.Spec.PMM, cr.Spec.Secrets.Users, okl && okp),
+			)
 		}
 	}
 
