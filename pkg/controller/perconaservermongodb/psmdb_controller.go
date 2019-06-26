4cd44083ffcc456fd97c5a3841abe6f36025f5bd
Max Dudin <dudin.mv@gmail.com>
2019-06-26 16:34:44 +0300

    chenge err handling

--- pkg/controller/perconaservermongodb/psmdb_controller.go
+++ pkg/controller/perconaservermongodb/psmdb_controller.go
@@ -456,7 +456,7 @@ func (r *ReconcilePerconaServerMongoDB) reconcileStatefulSet(arbiter bool, cr *a
 	sslInternalHash, err := r.getTLSHash(cr, cr.Spec.Secrets.SSLInternal)
 	if err != nil && !k8serrors.IsNotFound(err) {
 		return nil, fmt.Errorf("get secret hash error: %v", err)
-	} else if !k8serrors.IsNotFound(err) {
+	} else if err == nil {
 		sfsSpec.Template.Annotations["percona.com/ssl-internal-hash"] = sslInternalHash
 	}
 
