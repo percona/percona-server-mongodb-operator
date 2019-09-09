6281f6300b58ce0667bf4189ff674ae0dbec7887
Max Dudin <dudin.mv@gmail.com>
2019-09-09 13:48:57 +0300

    Add schedulerName

--- pkg/apis/psmdb/v1/psmdb_types.go
+++ pkg/apis/psmdb/v1/psmdb_types.go
@@ -59,6 +59,7 @@ type PerconaServerMongoDBSpec struct {
 	ImagePullPolicy  corev1.PullPolicy                    `json:"imagePullPolicy,omitempty"`
 	PMM              PMMSpec                              `json:"pmm,omitempty"`
 	UpdateStrategy   appsv1.StatefulSetUpdateStrategyType `json:"updateStrategy,omitempty"`
+	SchedulerName    string                               `json:"schedulerName,omitempty"`
 }
 
 type ReplsetMemberStatus struct {
