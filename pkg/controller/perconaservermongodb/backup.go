d9b4a8c2af943c5422412411caf8a5f3c58415f3
phin1x <fabian.weber14@outlook.de>
2020-04-27 14:44:34 +0200

    create a service account and a role binding for the backup job if the operator runs in cluster wide mode

--- pkg/controller/perconaservermongodb/backup.go
+++ pkg/controller/perconaservermongodb/backup.go
@@ -4,13 +4,21 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	batchv1b "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	api "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
 	"github.com/percona/percona-server-mongodb-operator/pkg/psmdb/backup"
+)
+
+const (
+	roleBindingName = "mongodb-operator"
 )
 
 func (r *ReconcilePerconaServerMongoDB) reconcileBackupTasks(cr *api.PerconaServerMongoDB) error {
@@ -58,6 +66,82 @@ func (r *ReconcilePerconaServerMongoDB) reconcileBackupTasks(cr *api.PerconaServ
 			err := r.client.Delete(context.TODO(), &t)
 			if err != nil && !errors.IsNotFound(err) {
 				return fmt.Errorf("delete backup task %s: %v", t.Name, err)
+			}
+		}
+	}
+
+	// no ns set, ignore it because we cannot run without it
+	ns, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		return err
+	}
+	if cr.Namespace != ns {
+		var sa corev1.ServiceAccount
+		if err := r.client.Get(context.TODO(), client.ObjectKey{
+			Namespace: cr.Namespace,
+			Name:      cr.Spec.Backup.ServiceAccountName,
+		}, &sa); err != nil {
+			if !errors.IsNotFound(err) {
+				return fmt.Errorf("get service account %s: %v", cr.Spec.Backup.ServiceAccountName, err)
+			}
+
+			sa = corev1.ServiceAccount{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "ServiceAccount",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      cr.Spec.Backup.ServiceAccountName,
+					Namespace: cr.Namespace,
+				},
+			}
+
+			if err := setControllerReference(cr, &sa, r.scheme); err != nil {
+				return fmt.Errorf("set owner reference for service account %s: %v", cr.Spec.Backup.ServiceAccountName, err)
+			}
+
+			if err := r.client.Create(context.TODO(), &sa); err != nil {
+				return fmt.Errorf("create service account %s: %v", cr.Spec.Backup.ServiceAccountName, err)
+			}
+		}
+
+		var rb rbacv1.RoleBinding
+		if err := r.client.Get(context.TODO(), client.ObjectKey{
+			Namespace: cr.Namespace,
+			Name:      roleBindingName,
+		}, &rb); err != nil {
+			if !errors.IsNotFound(err) {
+				return fmt.Errorf("get role binding %s: %v", roleBindingName, err)
+			}
+
+			rb = rbacv1.RoleBinding{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Kind:       "RoleBinding",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      roleBindingName,
+					Namespace: cr.Namespace,
+				},
+				RoleRef: rbacv1.RoleRef{
+					APIGroup: "rbac.authorization.k8s.io",
+					Kind:     "ClusterRole",
+					Name:     "mongodb-operator",
+				},
+				Subjects: []rbacv1.Subject{
+					{
+						Kind: "ServiceAccount",
+						Name: cr.Spec.Backup.ServiceAccountName,
+					},
+				},
+			}
+
+			if err := setControllerReference(cr, &rb, r.scheme); err != nil {
+				return fmt.Errorf("set owner reference for role binding %s: %v", roleBindingName, err)
+			}
+
+			if err := r.client.Create(context.TODO(), &rb); err != nil {
+				return fmt.Errorf("create role binding %s: %v", roleBindingName, err)
 			}
 		}
 	}
