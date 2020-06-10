7d96887cba3de472a397af06cad22298a5907b4c
paval <pavel.kasko@percona.com>
2020-06-10 12:37:48 +0300

    Fix ps-entry

--- pkg/psmdb/container.go
+++ pkg/psmdb/container.go
@@ -99,7 +99,6 @@ func container(m *api.PerconaServerMongoDB, replset *api.ReplsetSpec, name strin
 	}
 
 	if gte {
-		// container.Args = []string{"mongod"}
 		container.Command = []string{"/data/db/ps-entry.sh"}
 	}
 
