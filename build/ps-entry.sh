7126d78d96d71251b6a795114a6b1d3c7b2acb63
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2020-09-02 19:42:00 +0300

    PSMDB-686 add psmdb v4.4 support to ps-entry.sh

--- build/ps-entry.sh
+++ build/ps-entry.sh
@@ -395,7 +395,8 @@ if [ "$originalArgOne" = 'mongod' ]; then
 
 	MONGODB_VERSION=$(mongod --version  | head -1 | awk '{print $3}' | awk -F'.' '{print $1"."$2}')
 
-	if [ "$MONGODB_VERSION" == "v4.2" ]; then
+
+	if [ "$MONGODB_VERSION" == 'v4.2' ] || [ "$MONGODB_VERSION" == 'v4.4' ]; then
 		_mongod_hack_rename_arg_save_val --sslMode --tlsMode "${mongodHackedArgs[@]}"
 
 		if _mongod_hack_have_arg '--tlsMode' "${mongodHackedArgs[@]}"; then
