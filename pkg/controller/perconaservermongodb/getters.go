66b9eee3bb8a555e2ba3f9dd36c34fac851ab9b1
paval <pavel.kasko@percona.com>
2020-12-30 14:24:28 +0200

    Move labels to separate funcs

--- pkg/controller/perconaservermongodb/getters.go
+++ pkg/controller/perconaservermongodb/getters.go
@@ -14,14 +14,8 @@ func (r *ReconcilePerconaServerMongoDB) getMongosPods(cr *api.PerconaServerMongo
 	err := r.client.List(context.TODO(),
 		&mongosPods,
 		&client.ListOptions{
-			Namespace: cr.Namespace,
-			LabelSelector: labels.SelectorFromSet(map[string]string{
-				"app.kubernetes.io/name":       "percona-server-mongodb",
-				"app.kubernetes.io/instance":   cr.Name,
-				"app.kubernetes.io/managed-by": "percona-server-mongodb-operator",
-				"app.kubernetes.io/part-of":    "percona-server-mongodb",
-				"app.kubernetes.io/component":  "mongos",
-			}),
+			Namespace:     cr.Namespace,
+			LabelSelector: labels.SelectorFromSet(mongosLabels(cr)),
 		},
 	)
 
@@ -29,22 +23,34 @@ func (r *ReconcilePerconaServerMongoDB) getMongosPods(cr *api.PerconaServerMongo
 }
 
 func (r *ReconcilePerconaServerMongoDB) getRSPods(cr *api.PerconaServerMongoDB, rsName string) (corev1.PodList, error) {
-	matchLabels := map[string]string{
+	pods := corev1.PodList{}
+	err := r.client.List(context.TODO(),
+		&pods,
+		&client.ListOptions{
+			Namespace:     cr.Namespace,
+			LabelSelector: labels.SelectorFromSet(rsLabels(cr, rsName)),
+		},
+	)
+
+	return pods, err
+}
+
+func rsLabels(cr *api.PerconaServerMongoDB, rsName string) map[string]string {
+	return map[string]string{
 		"app.kubernetes.io/name":       "percona-server-mongodb",
 		"app.kubernetes.io/instance":   cr.Name,
 		"app.kubernetes.io/replset":    rsName,
 		"app.kubernetes.io/managed-by": "percona-server-mongodb-operator",
 		"app.kubernetes.io/part-of":    "percona-server-mongodb",
 	}
+}
 
-	pods := corev1.PodList{}
-	err := r.client.List(context.TODO(),
-		&pods,
-		&client.ListOptions{
-			Namespace:     cr.Namespace,
-			LabelSelector: labels.SelectorFromSet(matchLabels),
-		},
-	)
-
-	return pods, err
+func mongosLabels(cr *api.PerconaServerMongoDB) map[string]string {
+	return map[string]string{
+		"app.kubernetes.io/name":       "percona-server-mongodb",
+		"app.kubernetes.io/instance":   cr.Name,
+		"app.kubernetes.io/managed-by": "percona-server-mongodb-operator",
+		"app.kubernetes.io/part-of":    "percona-server-mongodb",
+		"app.kubernetes.io/component":  "mongos",
+	}
 }
