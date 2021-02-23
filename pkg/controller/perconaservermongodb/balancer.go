173bce08968ded67a8247c08edd9fa36c18f6c0c
Paval Kasko <pavel.kasko@percona.com>
2021-02-23 17:00:43 +0200

    Update pkg/controller/perconaservermongodb/balancer.go
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>

--- pkg/controller/perconaservermongodb/balancer.go
+++ pkg/controller/perconaservermongodb/balancer.go
@@ -54,20 +54,7 @@ func (r *ReconcilePerconaServerMongoDB) enableBalancerIfNeeded(cr *api.PerconaSe
 		return nil
 	}
 
-	mongosPods := corev1.PodList{}
-	err = r.client.List(context.TODO(),
-		&mongosPods,
-		&client.ListOptions{
-			Namespace: cr.Namespace,
-			LabelSelector: labels.SelectorFromSet(map[string]string{
-				"app.kubernetes.io/name":       "percona-server-mongodb",
-				"app.kubernetes.io/instance":   cr.Name,
-				"app.kubernetes.io/managed-by": "percona-server-mongodb-operator",
-				"app.kubernetes.io/part-of":    "percona-server-mongodb",
-				"app.kubernetes.io/component":  "mongos",
-			}),
-		},
-	)
+	mongosPods, err := r.getMongosPods(cr)
 	if err != nil && !k8sErrors.IsNotFound(err) {
 		return errors.Wrap(err, "get pods list for mongos")
 	}
