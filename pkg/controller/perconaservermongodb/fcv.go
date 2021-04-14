408a6527b80b4ea5e67904cc9e052eb022c74ce7
Paval Kasko <pavel.kasko@percona.com>
2021-04-14 09:42:56 +0300

    K8SPSMDB-284: Major Upgrade (#625)

---
+++ pkg/controller/perconaservermongodb/fcv.go
@@ -0,0 +1,47 @@
+package perconaservermongodb
+
+import (
+	"context"
+
+	v "github.com/hashicorp/go-version"
+	api "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
+	"github.com/percona/percona-server-mongodb-operator/pkg/psmdb/mongo"
+	"github.com/pkg/errors"
+	mgo "go.mongodb.org/mongo-driver/mongo"
+)
+
+func (r *ReconcilePerconaServerMongoDB) getFCV(cr *api.PerconaServerMongoDB) (string, error) {
+	c, err := r.mongoClientWithRole(cr, *cr.Spec.Replsets[0], roleClusterAdmin)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get connection")
+	}
+
+	return mongo.GetFCV(context.TODO(), c)
+
+}
+
+func (r *ReconcilePerconaServerMongoDB) setFCV(cr *api.PerconaServerMongoDB, version string) error {
+	if len(version) == 0 {
+		return errors.New("empty version")
+	}
+
+	v, err := v.NewSemver(version)
+	if err != nil {
+		return errors.Wrap(err, "failed to get go semver")
+	}
+
+	var cli *mgo.Client
+	var connErr error
+
+	if cr.Spec.Sharding.Enabled {
+		cli, connErr = r.mongosClientWithRole(cr, roleClusterAdmin)
+	} else {
+		cli, connErr = r.mongoClientWithRole(cr, *cr.Spec.Replsets[0], roleClusterAdmin)
+	}
+
+	if connErr != nil {
+		return errors.Wrap(connErr, "failed to get connection")
+	}
+
+	return mongo.SetFCV(context.TODO(), cli, MajorMinor(v))
+}
