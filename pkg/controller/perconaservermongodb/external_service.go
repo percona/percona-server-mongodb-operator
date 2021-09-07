0f65468b70634bef871802d0fadce1502fe7038b
Ege Güneş <ege.gunes@percona.com>
2021-09-07 12:58:30 +0300

    K8SPSMDB-550: Fix arbiter service if rs exposed

--- pkg/controller/perconaservermongodb/external_service.go
+++ pkg/controller/perconaservermongodb/external_service.go
@@ -53,6 +53,12 @@ func (r *ReconcilePerconaServerMongoDB) removeOutdatedServices(cr *api.PerconaSe
 		}
 	}
 
+	if replset.Arbiter.Enabled {
+		for i := 0; i < int(replset.Arbiter.Size); i++ {
+			svcNames[service.Name+"-arbiter-"+strconv.Itoa(i)] = struct{}{}
+		}
+	}
+
 	// clear old services
 	svcList := &corev1.ServiceList{}
 	err := r.client.List(context.TODO(),
