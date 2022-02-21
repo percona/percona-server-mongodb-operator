360c9bd6c7a76ca37b27501350882f27dadad6bf
Natalia Marukovich <nmarukovich@gmail.com>
2022-02-21 18:41:36 +0300

    fix cross-site tests

--- e2e-tests/cross-site-sharded/data.js
+++ e2e-tests/cross-site-sharded/data.js
@@ -2,7 +2,7 @@ db = db.getSiblingDB('app');
 
 var bulk = db.city.initializeUnorderedBulkOp();
 
-for (var i=0; i<3000000; i++) {
+for (var i=0; i<300000; i++) {
    bulk.insert({ "name": "city-"+i, "zipcode": i });
 }
 
