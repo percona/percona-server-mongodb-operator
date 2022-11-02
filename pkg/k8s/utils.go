cc06af22c7352d5d7e5f2c2a832ade9f1fce2cd3
Ivan Pylypenko <ivan.pylypenko@percona.com>
2022-11-02 23:46:47 +0200

    CLOUD-739 Watch the operators' namespace in any case (#1074)
    The bug in K8SPXC-1123 revealed the flaw in our cw operation mode -
    basically we ignoring the namespace where operator resides and it
    could lead to some issues. Fixing that.

--- pkg/k8s/utils.go
+++ pkg/k8s/utils.go
@@ -2,7 +2,9 @@ package k8s
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const WatchNamespaceEnvVar = "WATCH_NAMESPACE"
@@ -14,4 +16,14 @@ func GetWatchNamespace() (string, error) {
 		return "", fmt.Errorf("%s must be set", WatchNamespaceEnvVar)
 	}
 	return ns, nil
+}
+
+// GetOperatorNamespace returns the namespace of the operator pod
+func GetOperatorNamespace() (string, error) {
+	nsBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(nsBytes)), nil
 }
