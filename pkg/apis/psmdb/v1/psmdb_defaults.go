bb1620076d1b7aea08ed73df86e173985bbe963e
Vitaliy Orbidan <vitaliy_orbidan@comcast.com>
2023-02-07 11:55:32 -0700

    - Updated the info message in case when external nodes are present, but ReplSet is not exposed.

--- pkg/apis/psmdb/v1/psmdb_defaults.go
+++ pkg/apis/psmdb/v1/psmdb_defaults.go
@@ -436,7 +436,7 @@ func (cr *PerconaServerMongoDB) CheckNSetDefaults(platform version.Platform, log
 		}
 
 		if cr.Spec.Unmanaged && !replset.Expose.Enabled {
-			log.Info("consider exposing replset for unmanaged clusters", "replset", replset.Name)
+			log.Info("Replset is not exposed. Make sure each pod in the replset can reach each other.", "replset", replset.Name)
 		}
 
 		err := replset.SetDefaults(platform, cr, log)
@@ -593,7 +593,7 @@ func (rs *ReplsetSpec) SetDefaults(platform version.Platform, cr *PerconaServerM
 	}
 
 	if len(rs.ExternalNodes) > 0 && !rs.Expose.Enabled {
-		log.Info("consider exposing replicaset when adding external nodes", "replset", rs.Name)
+		log.Info("Replset is not exposed. Make sure each pod in the replset can reach each other.", "replset", rs.Name)
 	}
 
 	for _, extNode := range rs.ExternalNodes {
