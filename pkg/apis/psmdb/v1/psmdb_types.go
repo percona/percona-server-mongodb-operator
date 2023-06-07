afabd8010be5b626724e638dd6c59f0f396bbfcb
Ege Güneş <ege.gunes@percona.com>
2023-06-07 16:47:00 +0300

    K8SPSMDB-938: Allow configuring hostAliases for pods (#1236)
    * K8SPSMDB-938: Allow configuring hostAliases for pods
    
    * fix k8s version

--- pkg/apis/psmdb/v1/psmdb_types.go
+++ pkg/apis/psmdb/v1/psmdb_types.go
@@ -520,6 +520,7 @@ type ReplsetSpec struct {
 	Configuration            MongoConfiguration         `json:"configuration,omitempty"`
 	ExternalNodes            []*ExternalNode            `json:"externalNodes,omitempty"`
 	NonVoting                NonVotingSpec              `json:"nonvoting,omitempty"`
+	HostAliases              []corev1.HostAlias         `json:"hostAliases,omitempty"`
 }
 
 func (r *ReplsetSpec) ServiceName(cr *PerconaServerMongoDB) string {
@@ -619,6 +620,7 @@ type MongosSpec struct {
 	PodSecurityContext       *corev1.PodSecurityContext `json:"podSecurityContext,omitempty"`
 	ContainerSecurityContext *corev1.SecurityContext    `json:"containerSecurityContext,omitempty"`
 	Configuration            MongoConfiguration         `json:"configuration,omitempty"`
+	HostAliases              []corev1.HostAlias         `json:"hostAliases,omitempty"`
 }
 
 type MongodSpec struct {
