4cfadff383c5da9e3f8bc3c9df1ef27d1e9f2bff
Ege Güneş <ege.gunes@percona.com>
2023-07-12 11:56:43 +0300

    CUSTOM-147: Custom replset name, hostAliases, liveness probe fix (#1250)
    * K8SPSMDB-807: Allow customizing replset name with custom config
    
    * K8SPSMDB-937: Fix decoding initialSyncStatus to empty interface
    
    * K8SPSMDB-938: Allow configuring hostAliases for pods (#1236)
    
    * K8SPSMDB-938: Allow configuring hostAliases for pods
    
    * fix k8s version
    
    * CUSTOM-144 fix backups for custom replSetName (#1238)
    
    * fix
    
    * fix sharding
    
    ---------
    
    Co-authored-by: Andrii Dema <a.dema@jazzserve.com>
    
    * fix k8s version
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>
    Co-authored-by: Andrii Dema <a.dema@jazzserve.com>

--- pkg/controller/perconaservermongodb/mgo.go
+++ pkg/controller/perconaservermongodb/mgo.go
@@ -187,20 +187,26 @@ func (r *ReconcilePerconaServerMongoDB) reconcileCluster(ctx context.Context, cr
 			return api.AppStateError, errors.Wrap(err, "set default RW concern")
 		}
 
-		in, err := inShard(ctx, mongosSession, replset.Name)
+		rsName := replset.Name
+		name, err := replset.CustomReplsetName()
+		if err == nil {
+			rsName = name
+		}
+
+		in, err := inShard(ctx, mongosSession, rsName)
 		if err != nil {
 			return api.AppStateError, errors.Wrap(err, "get shard")
 		}
 
 		if !in {
-			log.Info("adding rs to shard", "rs", replset.Name)
+			log.Info("adding rs to shard", "rs", rsName)
 
 			err := r.handleRsAddToShard(ctx, cr, replset, pods.Items[0], mongosPods[0])
 			if err != nil {
 				return api.AppStateError, errors.Wrap(err, "add shard")
 			}
 
-			log.Info("added to shard", "rs", replset.Name)
+			log.Info("added to shard", "rs", rsName)
 		}
 
 		rs := cr.Status.Replsets[replset.Name]
@@ -503,7 +509,12 @@ func (r *ReconcilePerconaServerMongoDB) handleRsAddToShard(ctx context.Context, 
 		}
 	}()
 
-	err = mongo.AddShard(ctx, cli, replset.Name, host)
+	rsName := replset.Name
+	name, err := replset.CustomReplsetName()
+	if err == nil {
+		rsName = name
+	}
+	err = mongo.AddShard(ctx, cli, rsName, host)
 	if err != nil {
 		return errors.Wrap(err, "failed to add shard")
 	}
@@ -523,7 +534,13 @@ func (r *ReconcilePerconaServerMongoDB) handleReplsetInit(ctx context.Context, c
 			continue
 		}
 
-		log.Info("initiating replset", "replset", replset.Name, "pod", pod.Name)
+		replsetName := replset.Name
+		name, err := replset.CustomReplsetName()
+		if err == nil {
+			replsetName = name
+		}
+
+		log.Info("initiating replset", "replset", replsetName, "pod", pod.Name)
 
 		host, err := psmdb.MongoHost(ctx, r.client, cr, replset.Name, replset.Expose.Enabled, pod)
 		if err != nil {
@@ -560,7 +577,7 @@ func (r *ReconcilePerconaServerMongoDB) handleReplsetInit(ctx context.Context, c
 					}
 				)
 				EOF
-			`, mongoCmd, replset.Name, host),
+			`, mongoCmd, replsetName, host),
 		}
 
 		errb.Reset()
@@ -585,7 +602,7 @@ func (r *ReconcilePerconaServerMongoDB) handleReplsetInit(ctx context.Context, c
 			return fmt.Errorf("exec add admin user: %v / %s / %s", err, outb.String(), errb.String())
 		}
 
-		log.Info("replset initialized", "replset", replset.Name, "pod", pod.Name)
+		log.Info("replset initialized", "replset", replsetName, "pod", pod.Name)
 
 		return nil
 	}
