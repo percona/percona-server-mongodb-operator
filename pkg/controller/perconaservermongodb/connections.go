e0bced529e37d08467f48bb7b530ebfb7f0a351f
Ege Güneş <ege.gunes@percona.com>
2023-09-08 21:08:07 +0300

    K8SPSMDB-926: Implement PiTR for physical restores (#1288)
    * K8SPSMDB-926: Implement PiTR for physical restores
    
    * handle mongo version < 6.0
    
    * implement recovering to latest oplog chunk
    
    * add test
    
    * fixes
    
    * more fixes
    
    * use custom PBM image
    
    * fix backup image

--- pkg/controller/perconaservermongodb/connections.go
+++ pkg/controller/perconaservermongodb/connections.go
@@ -12,9 +12,9 @@ import (
 )
 
 type MongoClientProvider interface {
-	Mongo(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec, role UserRole) (mongo.Client, error)
-	Mongos(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole) (mongo.Client, error)
-	Standalone(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole, host string) (mongo.Client, error)
+	Mongo(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec, role api.UserRole) (mongo.Client, error)
+	Mongos(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole) (mongo.Client, error)
+	Standalone(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole, host string) (mongo.Client, error)
 }
 
 func (r *ReconcilePerconaServerMongoDB) MongoClientProvider() MongoClientProvider {
@@ -28,7 +28,7 @@ type mongoClientProvider struct {
 	k8sclient client.Client
 }
 
-func (p *mongoClientProvider) Mongo(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec, role UserRole) (mongo.Client, error) {
+func (p *mongoClientProvider) Mongo(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec, role api.UserRole) (mongo.Client, error) {
 	c, err := getInternalCredentials(ctx, p.k8sclient, cr, role)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get credentials")
@@ -37,7 +37,7 @@ func (p *mongoClientProvider) Mongo(ctx context.Context, cr *api.PerconaServerMo
 	return psmdb.MongoClient(ctx, p.k8sclient, cr, rs, c)
 }
 
-func (p *mongoClientProvider) Mongos(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole) (mongo.Client, error) {
+func (p *mongoClientProvider) Mongos(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole) (mongo.Client, error) {
 	c, err := getInternalCredentials(ctx, p.k8sclient, cr, role)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get credentials")
@@ -46,7 +46,7 @@ func (p *mongoClientProvider) Mongos(ctx context.Context, cr *api.PerconaServerM
 	return psmdb.MongosClient(ctx, p.k8sclient, cr, c)
 }
 
-func (p *mongoClientProvider) Standalone(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole, host string) (mongo.Client, error) {
+func (p *mongoClientProvider) Standalone(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole, host string) (mongo.Client, error) {
 	c, err := getInternalCredentials(ctx, p.k8sclient, cr, role)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get credentials")
@@ -55,15 +55,14 @@ func (p *mongoClientProvider) Standalone(ctx context.Context, cr *api.PerconaSer
 	return psmdb.StandaloneClient(ctx, p.k8sclient, cr, c, host)
 }
 
-func (r *ReconcilePerconaServerMongoDB) mongoClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec,
-	role UserRole) (mongo.Client, error) {
+func (r *ReconcilePerconaServerMongoDB) mongoClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, rs api.ReplsetSpec, role api.UserRole) (mongo.Client, error) {
 	return r.MongoClientProvider().Mongo(ctx, cr, rs, role)
 }
 
-func (r *ReconcilePerconaServerMongoDB) mongosClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole) (mongo.Client, error) {
+func (r *ReconcilePerconaServerMongoDB) mongosClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole) (mongo.Client, error) {
 	return r.MongoClientProvider().Mongos(ctx, cr, role)
 }
 
-func (r *ReconcilePerconaServerMongoDB) standaloneClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, role UserRole, host string) (mongo.Client, error) {
+func (r *ReconcilePerconaServerMongoDB) standaloneClientWithRole(ctx context.Context, cr *api.PerconaServerMongoDB, role api.UserRole, host string) (mongo.Client, error) {
 	return r.MongoClientProvider().Standalone(ctx, cr, role, host)
 }
