dcc31aeeb7bfd3adf3131969b9b93ef9dc40f52a
t-yrka <78016060+t-yrka@users.noreply.github.com>
2023-09-19 19:39:36 +0200

    K8SPSMDB-813: Fail TLS configuration if provided certificates do not exist (#1254)
    * Fail TLS configuration if provided certificates do not exist
    
    * Skip TLS config for probe when using unsafe config
    
    * Remove healthcheck ssl config from e2e where it was not expected
    
    * Revert ssl config removal for 'some-name' rs
    
    'allowUnsafeConfigurations' is set for 'another-name' rs only
    
    * compare version fixes
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>
    Co-authored-by: Andrii Dema <a.dema@jazzserve.com>
    Co-authored-by: Inel Pandzic <inel.pandzic@percona.com>

--- pkg/apis/psmdb/v1/psmdb_defaults.go
+++ pkg/apis/psmdb/v1/psmdb_defaults.go
@@ -191,7 +191,8 @@ func (cr *PerconaServerMongoDB) CheckNSetDefaults(platform version.Platform, log
 				},
 			}
 
-			if cr.CompareVersion("1.7.0") >= 0 {
+			if (cr.CompareVersion("1.7.0") >= 0 && cr.CompareVersion("1.15.0") < 0) ||
+				cr.CompareVersion("1.15.0") >= 0 && !cr.Spec.UnsafeConf {
 				cr.Spec.Sharding.Mongos.LivenessProbe.Exec.Command =
 					append(cr.Spec.Sharding.Mongos.LivenessProbe.Exec.Command,
 						"--ssl", "--sslInsecure",
@@ -236,7 +237,8 @@ func (cr *PerconaServerMongoDB) CheckNSetDefaults(platform version.Platform, log
 				},
 			}
 
-			if cr.CompareVersion("1.7.0") >= 0 {
+			if (cr.CompareVersion("1.7.0") >= 0 && cr.CompareVersion("1.15.0") < 0) ||
+				cr.CompareVersion("1.15.0") >= 0 && !cr.Spec.UnsafeConf {
 				cr.Spec.Sharding.Mongos.ReadinessProbe.Exec.Command =
 					append(cr.Spec.Sharding.Mongos.ReadinessProbe.Exec.Command,
 						"--ssl", "--sslInsecure",
@@ -362,7 +364,8 @@ func (cr *PerconaServerMongoDB) CheckNSetDefaults(platform version.Platform, log
 
 			if cr.CompareVersion("1.6.0") >= 0 {
 				replset.LivenessProbe.Probe.Exec.Command[0] = "/data/db/mongodb-healthcheck"
-				if cr.CompareVersion("1.7.0") >= 0 {
+				if (cr.CompareVersion("1.7.0") >= 0 && cr.CompareVersion("1.15.0") < 0) ||
+					cr.CompareVersion("1.15.0") >= 0 && !cr.Spec.UnsafeConf {
 					replset.LivenessProbe.Probe.Exec.Command =
 						append(replset.LivenessProbe.Probe.Exec.Command,
 							"--ssl", "--sslInsecure",
@@ -643,14 +646,14 @@ func (nv *NonVotingSpec) SetDefaults(cr *PerconaServerMongoDB, rs *ReplsetSpec) 
 	}
 	if nv.LivenessProbe.ProbeHandler.Exec == nil {
 		nv.LivenessProbe.Probe.ProbeHandler.Exec = &corev1.ExecAction{
-			Command: []string{
-				"/data/db/mongodb-healthcheck",
-				"k8s",
-				"liveness",
-				"--ssl", "--sslInsecure",
-				"--sslCAFile", "/etc/mongodb-ssl/ca.crt",
-				"--sslPEMKeyFile", "/tmp/tls.pem",
-			},
+			Command: []string{"/data/db/mongodb-healthcheck", "k8s", "liveness"},
+		}
+
+		if !cr.Spec.UnsafeConf || cr.CompareVersion("1.15.0") < 0 {
+			nv.LivenessProbe.Probe.ProbeHandler.Exec.Command = append(
+				nv.LivenessProbe.Probe.ProbeHandler.Exec.Command,
+				"--ssl", "--sslInsecure", "--sslCAFile", "/etc/mongodb-ssl/ca.crt", "--sslPEMKeyFile", "/tmp/tls.pem",
+			)
 		}
 
 		if cr.CompareVersion("1.14.0") >= 0 {
