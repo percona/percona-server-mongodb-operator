1ff51e9c81216b731619b5998992851dba4dfd29
Andrii Dema <a.dema@jazzserve.com>
2024-03-25 22:26:07 +0200

    small fix

--- build/ps-entry.sh
+++ build/ps-entry.sh
@@ -432,7 +432,7 @@ if [[ $originalArgOne == mongo* ]]; then
 	fi
 	LDAP_SSL_DIR=${LDAP_SSL_DIR:-/etc/openldap/certs}
 	if [ -f "${LDAP_SSL_DIR}/ca.crt" ]; then
-		echo "TLS_CACERT ${LDAP_SSL_DIR}/ca.crt" >>/etc/openldap/ldap.conf
+		echo "TLS_CACERT ${LDAP_SSL_DIR}/ca.crt" >/etc/openldap/ldap.conf
 	fi
 	if [ -f "${MONGO_SSL_DIR}/tls.key" ] && [ -f "${MONGO_SSL_DIR}/tls.crt" ]; then
 		cat "${MONGO_SSL_DIR}/tls.key" "${MONGO_SSL_DIR}/tls.crt" >/tmp/tls.pem
