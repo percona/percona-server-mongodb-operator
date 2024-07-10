633c872e253981df6cf51da29320ecaaba13dc5e
Andrii Dema <a.dema@jazzserve.com>
2024-07-10 15:50:10 +0300

    remove unused env vars

--- cmd/mongodb-healthcheck/tool/env.go
+++ cmd/mongodb-healthcheck/tool/env.go
@@ -15,37 +15,16 @@
 package tool
 
 const (
-	// general
-	EnvServiceName    = "SERVICE_NAME"
 	EnvMongoDBPort    = "MONGODB_PORT"
-	EnvMongoDBIp      = "MONGODB_IP"
 	EnvMongoDBReplset = "MONGODB_REPLSET"
-
-	// backup user
-	EnvMongoDBBackupUser     = "MONGODB_BACKUP_USER"
-	EnvMongoDBBackupPassword = "MONGODB_BACKUP_PASSWORD"
-
-	// clusterAdmin user
-	EnvMongoDBClusterAdminUser     = "MONGODB_CLUSTER_ADMIN_USER"
-	EnvMongoDBClusterAdminPassword = "MONGODB_CLUSTER_ADMIN_PASSWORD"
 
 	// clusterMonitor user
 	EnvMongoDBClusterMonitorUser     = "MONGODB_CLUSTER_MONITOR_USER"
 	EnvMongoDBClusterMonitorPassword = "MONGODB_CLUSTER_MONITOR_PASSWORD"
-
-	// userAdmin user
-	EnvMongoDBUserAdminUser     = "MONGODB_USER_ADMIN_USER"
-	EnvMongoDBUserAdminPassword = "MONGODB_USER_ADMIN_PASSWORD"
 
 	// mongodb ssl
 	EnvMongoDBNetSSLEnabled    = "MONGODB_NET_SSL_ENABLED"
 	EnvMongoDBNetSSLInsecure   = "MONGODB_NET_SSL_INSECURE"
 	EnvMongoDBNetSSLPEMKeyFile = "MONGODB_NET_SSL_PEM_KEY_FILE"
 	EnvMongoDBNetSSLCAFile     = "MONGODB_NET_SSL_CA_FILE"
-
-	// replset init
-	EnvInitInitiateDelay       = "INIT_INITIATE_DELAY"
-	EnvInitMaxConnectTries     = "INIT_MAX_CONNECT_TRIES"
-	EnvInitMaxInitReplsetTries = "INIT_MAX_INIT_REPLSET_TRIES"
-	EnvInitRetrySleep          = "INIT_RETRY_SLEEP"
 )
