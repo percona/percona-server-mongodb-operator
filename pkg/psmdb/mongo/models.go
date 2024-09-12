48e77d598a6665f7a86883f568fefe9ec299739a
Inel Pandzic <inel.pandzic@percona.com>
2024-09-12 11:38:30 +0200

    Update pkg/psmdb/mongo/models.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/psmdb/mongo/models.go
+++ pkg/psmdb/mongo/models.go
@@ -252,7 +252,7 @@ type RoleInfo struct {
 }
 
 type User struct {
-	DB   string `bson:"db" json:"db"`
+	DB    string                   `bson:"db" json:"db"`
 	Roles []map[string]interface{} `bson:"roles" json:"roles"`
 }
 
