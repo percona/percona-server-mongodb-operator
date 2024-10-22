159e8f355e3c9238662451931fe15f491cecd525
Inel Pandzic <inel.pandzic@percona.com>
2024-10-22 14:10:24 +0200

    K8SPSMDB-1146: Fix roles comparison (#1679)
    * Fix roles check for update logic.
    
    * Fix user role comparation.
    
    * log
    
    * Log
    
    * Fix logic.
    
    * Fix
    
    * Log
    
    * Log
    
    * fix
    
    * Get the value for cluster.
    
    * Cleanup
    
    * Validate present user roles.
    
    * Update k8s version to 1.28.
    
    * Update pkg/controller/perconaservermongodb/mgo.go
    
    Co-authored-by: Andrii Dema <a.dema@jazzserve.com>
    
    ---------
    
    Co-authored-by: Andrii Dema <a.dema@jazzserve.com>
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- pkg/controller/perconaservermongodb/mgo.go
+++ pkg/controller/perconaservermongodb/mgo.go
@@ -816,17 +816,17 @@ func compareSlices(x, y []string) bool {
 	return true
 }
 
-// comparePrivileges compares 2 RolePrivilege arrays and returns true if they are equal
-func comparePrivileges(x []mongo.RolePrivilege, y []mongo.RolePrivilege) bool {
+// privilegesChanged compares 2 RolePrivilege arrays and returns true if they are not equal
+func privilegesChanged(x []mongo.RolePrivilege, y []mongo.RolePrivilege) bool {
 	if len(x) != len(y) {
-		return false
+		return true
 	}
 	for i := range x {
-		if !(compareResources(x[i].Resource, y[i].Resource) && compareSlices(x[i].Actions, y[i].Actions)) {
-			return false
+		if !compareResources(x[i].Resource, y[i].Resource) || !compareSlices(x[i].Actions, y[i].Actions) {
+			return true
 		}
 	}
-	return true
+	return false
 }
 
 func compareTags(tags mongo.ReplsetTags, selector api.PrimaryPreferTagSelectorSpec) bool {
@@ -858,7 +858,7 @@ func (r *ReconcilePerconaServerMongoDB) createOrUpdateSystemRoles(ctx context.Co
 		err = cli.CreateRole(ctx, "admin", mo)
 		return errors.Wrapf(err, "create role %s", role)
 	}
-	if !comparePrivileges(privileges, roleInfo.Privileges) {
+	if privilegesChanged(privileges, roleInfo.Privileges) {
 		err = cli.UpdateRole(ctx, "admin", mo)
 		return errors.Wrapf(err, "update role")
 	}
