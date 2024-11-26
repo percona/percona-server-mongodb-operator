437ac69df1c265831f1af10db3be2d71053a99c7
Natalia Marukovich <nmarukovich@gmail.com>
2024-11-26 15:28:43 +0100

    K8SPSMDB-1075 use controller runtime builder (#1692)
    * K8SPSMDB-1075 use controller runtime builder
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * delete unused code
    
    * fix PR comments
    
    * fix controller
    
    * Update pkg/controller/perconaservermongodbbackup/perconaservermongodbbackup_controller.go
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix imports
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/controller/perconaservermongodbbackup/perconaservermongodbbackup_controller.go
+++ pkg/controller/perconaservermongodbbackup/perconaservermongodbbackup_controller.go
@@ -14,20 +14,18 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	pbmBackup "github.com/percona/percona-backup-mongodb/pbm/backup"
 	pbmErrors "github.com/percona/percona-backup-mongodb/pbm/errors"
 	"github.com/percona/percona-backup-mongodb/pbm/storage"
 	"github.com/percona/percona-backup-mongodb/pbm/storage/azure"
 	"github.com/percona/percona-backup-mongodb/pbm/storage/s3"
-
 	"github.com/percona/percona-server-mongodb-operator/clientcmd"
 	psmdbv1 "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
 	"github.com/percona/percona-server-mongodb-operator/pkg/naming"
@@ -67,29 +65,20 @@ func newReconciler(mgr manager.Manager) (reconcile.Reconciler, error) {
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
+
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
-	c, err := controller.New("psmdbbackup-controller", mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to primary resource PerconaServerMongoDBBackup
-	err = c.Watch(source.Kind(mgr.GetCache(), &psmdbv1.PerconaServerMongoDBBackup{}, &handler.TypedEnqueueRequestForObject[*psmdbv1.PerconaServerMongoDBBackup]{}))
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner PerconaServerMongoDBBackup
-	err = c.Watch(source.Kind(mgr.GetCache(), &corev1.Pod{}, handler.TypedEnqueueRequestForOwner[*corev1.Pod](
-		mgr.GetScheme(), mgr.GetRESTMapper(), &psmdbv1.PerconaServerMongoDBBackup{}, handler.OnlyControllerOwner(),
-	)))
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return builder.ControllerManagedBy(mgr).
+		Named("psmdbbackup-controller").
+		For(&psmdbv1.PerconaServerMongoDBBackup{}).
+		Watches(
+			&corev1.Pod{},
+			handler.EnqueueRequestForOwner(
+				mgr.GetScheme(), mgr.GetRESTMapper(),
+				&psmdbv1.PerconaServerMongoDBBackup{},
+				handler.OnlyControllerOwner(),
+			),
+		).
+		Complete(r)
 }
 
 var _ reconcile.Reconciler = &ReconcilePerconaServerMongoDBBackup{}
@@ -176,15 +165,6 @@ func (r *ReconcilePerconaServerMongoDBBackup) Reconcile(ctx context.Context, req
 		err = cluster.CheckNSetDefaults(svr.Platform, log)
 		if err != nil {
 			return rr, errors.Wrapf(err, "set defaults for %s/%s", cluster.Namespace, cluster.Name)
-		}
-		// TODO: Remove after 1.15
-		if cluster.CompareVersion("1.12.0") >= 0 && cr.Spec.ClusterName == "" {
-			cr.Spec.ClusterName = cr.Spec.PSMDBCluster
-			cr.Spec.PSMDBCluster = ""
-			err = r.client.Update(ctx, cr)
-			if err != nil {
-				return rr, errors.Wrap(err, "failed to update clusterName")
-			}
 		}
 	}
 
