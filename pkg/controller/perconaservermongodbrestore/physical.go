788505c164b190df77fc49d26c008f3b43542003
Andrii Dema <a.dema@jazzserve.com>
2025-05-27 10:02:20 +0300

    backoff wait after adding resync

--- pkg/controller/perconaservermongodbrestore/physical.go
+++ pkg/controller/perconaservermongodbrestore/physical.go
@@ -400,7 +400,6 @@ func (r *ReconcilePerconaServerMongoDBRestore) finishPhysicalRestore(ctx context
 		return false, nil
 	}
 
-	resyncPresent := false
 	if err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
 		c := new(psmdbv1.PerconaServerMongoDB)
 		if err := r.client.Get(ctx, client.ObjectKeyFromObject(cluster), c); err != nil {
@@ -411,7 +410,6 @@ func (r *ReconcilePerconaServerMongoDBRestore) finishPhysicalRestore(ctx context
 			c.Annotations = make(map[string]string)
 		}
 		if c.Annotations[psmdbv1.AnnotationResyncPBM] == "true" {
-			resyncPresent = true
 			return nil
 		}
 
@@ -421,8 +419,27 @@ func (r *ReconcilePerconaServerMongoDBRestore) finishPhysicalRestore(ctx context
 	}); err != nil {
 		return false, errors.Wrapf(err, "annotate psmdb/%s for PBM resync", cluster.Name)
 	}
-	if !resyncPresent {
-		return false, nil
+
+	err = retry.OnError(retry.DefaultBackoff, func(err error) bool {
+		return err != nil
+	}, func() error {
+		c := new(psmdbv1.PerconaServerMongoDB)
+		if err := r.client.Get(ctx, client.ObjectKeyFromObject(cluster), c); err != nil {
+			return err
+		}
+
+		if c.Annotations == nil {
+			return errors.New("annotation wasn't added")
+		}
+
+		if v, ok := c.Annotations[psmdbv1.AnnotationResyncPBM]; !ok || v != "true" {
+			return errors.New("annotation wasn't added 2")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return false, errors.Wrap(err, "failed to check if annotation was added")
 	}
 
 	if err := r.updateMongodSts(ctx, cluster, func(sts *appsv1.StatefulSet) error {
