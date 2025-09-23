b7762dae9f34c5ea9bb890717eecd31f4fe3776f
George Kechagias <george.kechagias@percona.com>
2025-09-23 12:19:26 +0300

    improve unit test

--- pkg/controller/perconaservermongodb/version_test.go
+++ pkg/controller/perconaservermongodb/version_test.go
@@ -11,6 +11,7 @@ import (
 	pbVersion "github.com/Percona-Lab/percona-version-service/versionpb"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -759,11 +760,12 @@ func (b *fakeVS) Apply(_ context.Context, req *pbVersion.ApplyRequest) (*pbVersi
 func TestVersionService(t *testing.T) {
 	vs := VersionServiceClient{}
 	tests := []struct {
-		cr        api.PerconaServerMongoDB
-		name      string
-		vm        VersionMeta
-		want      DepVersion
-		shouldErr bool
+		cr            api.PerconaServerMongoDB
+		name          string
+		vm            VersionMeta
+		want          DepVersion
+		expectedError error
+		isPMM3        bool
 	}{
 		{
 			name: "UpgradeOptions.Apply: disabled",
@@ -796,15 +798,16 @@ func TestVersionService(t *testing.T) {
 			want: DepVersion{},
 		},
 		{
-			name:      "Error on empty version service response",
-			cr:        api.PerconaServerMongoDB{},
-			vm:        VersionMeta{},
-			want:      DepVersion{},
-			shouldErr: true,
+			name:          "Error on empty version service response",
+			cr:            api.PerconaServerMongoDB{},
+			vm:            VersionMeta{},
+			want:          DepVersion{},
+			expectedError: errors.New("failed to version service apply"),
 		},
 		{
-			name: "Request to version service",
-			cr:   api.PerconaServerMongoDB{},
+			name:   "Request to version service with PMM3",
+			cr:     api.PerconaServerMongoDB{},
+			isPMM3: true,
 			vm: VersionMeta{
 				Apply:                   "",
 				MongoVersion:            "database-version",
@@ -835,6 +838,39 @@ func TestVersionService(t *testing.T) {
 				PMMVersion:    "3.1",
 			},
 		},
+		{
+			name: "Request to version service with PMM2",
+			cr:   api.PerconaServerMongoDB{},
+			vm: VersionMeta{
+				Apply:                   "",
+				MongoVersion:            "database-version",
+				KubeVersion:             "kube-version",
+				Platform:                productName,
+				PMMVersion:              "3.1",
+				BackupVersion:           "backup-version",
+				CRUID:                   "custom-resource-uid",
+				Version:                 version.Version(),
+				ClusterWideEnabled:      true,
+				HashicorpVaultEnabled:   true,
+				ShardingEnabled:         true,
+				PMMEnabled:              true,
+				HelmDeployOperator:      true,
+				HelmDeployCR:            true,
+				SidecarsUsed:            true,
+				BackupsEnabled:          true,
+				ClusterSize:             3,
+				PITREnabled:             true,
+				PhysicalBackupScheduled: true,
+			},
+			want: DepVersion{
+				MongoImage:    "mongo-image",
+				MongoVersion:  "mongo-version",
+				BackupImage:   "backup-image",
+				BackupVersion: "backup-version",
+				PMMImage:      "pmm2-image",
+				PMMVersion:    "2.1",
+			},
+		},
 	}
 	addr := "127.0.0.1"
 	port := 10000
@@ -844,18 +880,16 @@ func TestVersionService(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			dv, err := vs.GetExactVersion(&tt.cr, fmt.Sprintf("http://%s:%d", addr, gwPort), tt.vm, versionOptions{
-				PMM3Enabled: true,
-			})
-			if err != nil {
-				if tt.shouldErr {
-					return
-				}
-				require.NoError(t, err)
+			opts := versionOptions{
+				PMM3Enabled: tt.isPMM3,
 			}
-			if dv != tt.want {
-				t.Fatal(errors.Errorf("Have: %v; Want: %v", dv, tt.want))
+			dv, err := vs.GetExactVersion(&tt.cr, fmt.Sprintf("http://%s:%d", addr, gwPort), tt.vm, opts)
+			if tt.expectedError != nil {
+				assert.ErrorContains(t, err, tt.expectedError.Error())
+				return
 			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, dv)
 		})
 	}
 }
