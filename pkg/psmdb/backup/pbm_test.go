87fa41e2694a79384dd2289550255c84b145bf36
Ege Güneş <ege.gunes@percona.com>
2025-10-01 11:19:46 +0300

    K8SPSMDB-1412: PBM v2.10.0 fixes (#2071)
    * K8SPSMDB-1412: Fix GCS backup destination
    
    * K8SPSMDB-1412: Only configure native GCS if crVersion >= 1.21.0
    
    fix unit test
    
    * Apply suggestion from @github-actions[bot]
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: George Kechagias <geo.kechagias@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/psmdb/backup/pbm_test.go
+++ pkg/psmdb/backup/pbm_test.go
@@ -21,7 +21,9 @@ import (
 	"github.com/percona/percona-backup-mongodb/pbm/storage/fs"
 	"github.com/percona/percona-backup-mongodb/pbm/storage/gcs"
 	"github.com/percona/percona-backup-mongodb/pbm/storage/s3"
+
 	api "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
+	"github.com/percona/percona-server-mongodb-operator/pkg/version"
 )
 
 func TestApplyCustomPBMConfig(t *testing.T) {
@@ -112,6 +114,9 @@ func TestPBMStorageConfig(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test-cr",
 			Namespace: "test-namespace",
+		},
+		Spec: api.PerconaServerMongoDBSpec{
+			CRVersion: version.Version(),
 		},
 	}
 
