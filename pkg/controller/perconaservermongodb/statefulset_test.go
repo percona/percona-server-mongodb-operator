979fa2569ce24752a615b6a29c37e8f16064450b
Mayank Shah <mayank.shah@percona.com>
2025-12-22 17:42:19 +0530

    add unit test
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- pkg/controller/perconaservermongodb/statefulset_test.go
+++ pkg/controller/perconaservermongodb/statefulset_test.go
@@ -12,12 +12,14 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/yaml"
 
 	api "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
 	"github.com/percona/percona-server-mongodb-operator/pkg/naming"
 	"github.com/percona/percona-server-mongodb-operator/pkg/psmdb/logcollector"
+	"github.com/percona/percona-server-mongodb-operator/pkg/psmdb/logcollector/logrotate"
 	"github.com/percona/percona-server-mongodb-operator/pkg/version"
 )
 
@@ -42,11 +44,13 @@ func TestReconcileStatefulSet(t *testing.T) {
 	}
 
 	tests := []struct {
-		name      string
-		cr        *api.PerconaServerMongoDB
-		rsName    string
-		component string
-		ls        map[string]string
+		name          string
+		cr            *api.PerconaServerMongoDB
+		rsName        string
+		component     string
+		ls            map[string]string
+		crUpdate      func(cr *api.PerconaServerMongoDB)
+		additonalObjs []client.Object
 
 		expectedSts *appsv1.StatefulSet
 	}{
@@ -106,34 +110,84 @@ func TestReconcileStatefulSet(t *testing.T) {
 			component:   naming.ComponentHidden,
 			expectedSts: expectedSts(t, "reconcile-statefulset/cfg-hidden.yaml"),
 		},
+		{
+			name:        "rs0-logrotate",
+			cr:          defaultCR.DeepCopy(),
+			rsName:      "rs0",
+			component:   naming.ComponentMongod,
+			expectedSts: expectedSts(t, "reconcile-statefulset/rs0-logrotate.yaml"),
+			crUpdate: func(cr *api.PerconaServerMongoDB) {
+				cr.Spec.LogCollector.LogRotate = &api.LogRotateSpec{
+					Configuration: "test-config",
+					ExtraConfig: corev1.LocalObjectReference{
+						Name: "extra-config",
+					},
+					Schedule: "0 0 */2 * *",
+				}
+			},
+			additonalObjs: []client.Object{
+				&corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      logrotate.ConfigMapName(crName),
+						Namespace: ns,
+					},
+					Data: map[string]string{
+						"custom.conf": "custom-config",
+					},
+				},
+				&corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "extra-config",
+						Namespace: ns,
+					},
+					Data: map[string]string{
+						"custom.conf": "custom-config",
+					},
+				},
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := buildFakeClient(tt.cr, &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      crName + "-ssl",
-					Namespace: tt.cr.Namespace,
+
+			mockObjs := []client.Object{
+				tt.cr,
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      crName + "-ssl",
+						Namespace: tt.cr.Namespace,
+					},
+					Data: map[string][]byte{
+						"ca.crt":  []byte("fake-ca-cert"),
+						"tls.crt": []byte("fake-tls-cert"),
+						"tls.key": []byte("fake-tls-key"),
+					},
 				},
-				Data: map[string][]byte{
-					"ca.crt":  []byte("fake-ca-cert"),
-					"tls.crt": []byte("fake-tls-cert"),
-					"tls.key": []byte("fake-tls-key"),
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      crName + "-ssl-internal",
+						Namespace: tt.cr.Namespace,
+					},
 				},
-			}, &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      crName + "-ssl-internal",
-					Namespace: tt.cr.Namespace,
+
+				&corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      logcollector.ConfigMapName(tt.cr.Name),
+						Namespace: tt.cr.Namespace,
+					},
+					Data: map[string]string{
+						"fluentbit_custom.conf": "config",
+					},
 				},
-			}, &corev1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      logcollector.ConfigMapName(tt.cr.Name),
-					Namespace: tt.cr.Namespace,
-				},
-				Data: map[string]string{
-					"fluentbit_custom.conf": "config",
-				},
-			})
+			}
+
+			mockObjs = append(mockObjs, tt.additonalObjs...)
+			r := buildFakeClient(mockObjs...)
+
+			if tt.crUpdate != nil {
+				tt.crUpdate(tt.cr)
+			}
 
 			rs := tt.cr.Spec.Replset(tt.rsName)
 
