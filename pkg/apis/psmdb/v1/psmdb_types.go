1472e05343750e54194dd714fe4b626702a7ccbe
Natalia Marukovich <natalia.marukovich@percona.com>
2025-12-30 10:46:07 +0100

    unused

--- pkg/apis/psmdb/v1/psmdb_types.go
+++ pkg/apis/psmdb/v1/psmdb_types.go
@@ -1048,20 +1048,19 @@ type S3Retryer struct {
 }
 
 type BackupStorageS3Spec struct {
-	Bucket                string                    `json:"bucket"`
-	Prefix                string                    `json:"prefix,omitempty"`
-	Region                string                    `json:"region,omitempty"`
-	EndpointURL           string                    `json:"endpointUrl,omitempty"`
-	CredentialsSecret     string                    `json:"credentialsSecret,omitempty"`
-	UploadPartSize        int                       `json:"uploadPartSize,omitempty"`
-	MaxUploadParts        int32                     `json:"maxUploadParts,omitempty"`
-	StorageClass          string                    `json:"storageClass,omitempty"`
-	InsecureSkipTLSVerify bool                      `json:"insecureSkipTLSVerify,omitempty"`
-	CABundle              *corev1.SecretKeySelector `json:"caBundle,omitempty"`
-	ForcePathStyle        *bool                     `json:"forcePathStyle,omitempty"`
-	DebugLogLevels        string                    `json:"debugLogLevels,omitempty"`
-	Retryer               *S3Retryer                `json:"retryer,omitempty"`
-	ServerSideEncryption  S3ServiceSideEncryption   `json:"serverSideEncryption,omitempty"`
+	Bucket                string                  `json:"bucket"`
+	Prefix                string                  `json:"prefix,omitempty"`
+	Region                string                  `json:"region,omitempty"`
+	EndpointURL           string                  `json:"endpointUrl,omitempty"`
+	CredentialsSecret     string                  `json:"credentialsSecret,omitempty"`
+	UploadPartSize        int                     `json:"uploadPartSize,omitempty"`
+	MaxUploadParts        int32                   `json:"maxUploadParts,omitempty"`
+	StorageClass          string                  `json:"storageClass,omitempty"`
+	InsecureSkipTLSVerify bool                    `json:"insecureSkipTLSVerify,omitempty"`
+	ForcePathStyle        *bool                   `json:"forcePathStyle,omitempty"`
+	DebugLogLevels        string                  `json:"debugLogLevels,omitempty"`
+	Retryer               *S3Retryer              `json:"retryer,omitempty"`
+	ServerSideEncryption  S3ServiceSideEncryption `json:"serverSideEncryption,omitempty"`
 }
 
 type BackupStorageMinioSpec struct {
