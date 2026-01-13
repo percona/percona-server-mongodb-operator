3d24e22545f06158eac2e942c524ce8ec05b6394
George Kechagias <george.kechagias@percona.com>
2026-01-13 14:00:03 +0200

    add unit test for trigger resize

--- pkg/controller/perconaservermongodb/volume_autoscaling_test.go
+++ pkg/controller/perconaservermongodb/volume_autoscaling_test.go
@@ -5,10 +5,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
+	"github.com/percona/percona-server-mongodb-operator/pkg/apis"
 	api "github.com/percona/percona-server-mongodb-operator/pkg/apis/psmdb/v1"
 )
 
@@ -285,6 +289,236 @@ func TestFindPodByName(t *testing.T) {
 			} else {
 				assert.Nil(t, pod)
 			}
+		})
+	}
+}
+
+func TestTriggerResize(t *testing.T) {
+	ctx := context.Background()
+
+	tests := map[string]struct {
+		cr             *api.PerconaServerMongoDB
+		pvc            *corev1.PersistentVolumeClaim
+		newSize        resource.Quantity
+		expectedResize int32
+	}{
+		"successful resize for replset": {
+			cr: &api.PerconaServerMongoDB{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cluster",
+					Namespace: "default",
+				},
+				Spec: api.PerconaServerMongoDBSpec{
+					Replsets: []*api.ReplsetSpec{
+						{
+							Name: "rs0",
+							VolumeSpec: &api.VolumeSpec{
+								PersistentVolumeClaim: api.PVCSpec{
+									PersistentVolumeClaimSpec: &corev1.PersistentVolumeClaimSpec{
+										Resources: corev1.VolumeResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceStorage: resource.MustParse("10Gi"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mongod-data-test-cluster-rs0-0",
+					Namespace: "default",
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("10Gi"),
+					},
+				},
+			},
+			newSize:        resource.MustParse("15Gi"),
+			expectedResize: 1,
+		},
+		"successful resize for sharding config": {
+			cr: &api.PerconaServerMongoDB{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cluster",
+					Namespace: "default",
+				},
+				Spec: api.PerconaServerMongoDBSpec{
+					Sharding: api.Sharding{
+						Enabled: true,
+						ConfigsvrReplSet: &api.ReplsetSpec{
+							Name: "cfg",
+							VolumeSpec: &api.VolumeSpec{
+								PersistentVolumeClaim: api.PVCSpec{
+									PersistentVolumeClaimSpec: &corev1.PersistentVolumeClaimSpec{
+										Resources: corev1.VolumeResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceStorage: resource.MustParse("5Gi"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mongod-data-test-cluster-cfg-0",
+					Namespace: "default",
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("5Gi"),
+					},
+				},
+			},
+			newSize:        resource.MustParse("8Gi"),
+			expectedResize: 1,
+		},
+		"multiple resizes increment counter": {
+			cr: &api.PerconaServerMongoDB{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cluster",
+					Namespace: "default",
+				},
+				Spec: api.PerconaServerMongoDBSpec{
+					Replsets: []*api.ReplsetSpec{
+						{
+							Name: "rs0",
+							VolumeSpec: &api.VolumeSpec{
+								PersistentVolumeClaim: api.PVCSpec{
+									PersistentVolumeClaimSpec: &corev1.PersistentVolumeClaimSpec{
+										Resources: corev1.VolumeResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceStorage: resource.MustParse("10Gi"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Status: api.PerconaServerMongoDBStatus{
+					StorageAutoscaling: map[string]api.StorageAutoscalingStatus{
+						"mongod-data-test-cluster-rs0-0": {
+							ResizeCount: 2,
+						},
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mongod-data-test-cluster-rs0-0",
+					Namespace: "default",
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("10Gi"),
+					},
+				},
+			},
+			newSize:        resource.MustParse("15Gi"),
+			expectedResize: 3,
+		},
+		"resize with multiple replsets": {
+			cr: &api.PerconaServerMongoDB{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-cluster",
+					Namespace: "default",
+				},
+				Spec: api.PerconaServerMongoDBSpec{
+					Replsets: []*api.ReplsetSpec{
+						{
+							Name: "rs0",
+							VolumeSpec: &api.VolumeSpec{
+								PersistentVolumeClaim: api.PVCSpec{
+									PersistentVolumeClaimSpec: &corev1.PersistentVolumeClaimSpec{
+										Resources: corev1.VolumeResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceStorage: resource.MustParse("10Gi"),
+											},
+										},
+									},
+								},
+							},
+						},
+						{
+							Name: "rs1",
+							VolumeSpec: &api.VolumeSpec{
+								PersistentVolumeClaim: api.PVCSpec{
+									PersistentVolumeClaimSpec: &corev1.PersistentVolumeClaimSpec{
+										Resources: corev1.VolumeResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceStorage: resource.MustParse("10Gi"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mongod-data-test-cluster-rs0-0",
+					Namespace: "default",
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("10Gi"),
+					},
+				},
+			},
+			newSize:        resource.MustParse("15Gi"),
+			expectedResize: 1,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			s := scheme.Scheme
+			err := apis.AddToScheme(s)
+			require.NoError(t, err)
+
+			fakeClient := fake.NewClientBuilder().
+				WithScheme(s).
+				WithObjects(tt.cr).
+				Build()
+
+			r := &ReconcilePerconaServerMongoDB{
+				client: fakeClient,
+			}
+
+			var volumeSpec *api.VolumeSpec
+			if len(tt.cr.Spec.Replsets) > 0 {
+				volumeSpec = tt.cr.Spec.Replsets[0].VolumeSpec
+			} else if tt.cr.Spec.Sharding.Enabled {
+				volumeSpec = tt.cr.Spec.Sharding.ConfigsvrReplSet.VolumeSpec
+			}
+			require.NotNil(t, volumeSpec)
+
+			originalSize := volumeSpec.PersistentVolumeClaim.Resources.Requests[corev1.ResourceStorage]
+
+			err = r.triggerResize(ctx, tt.cr, tt.pvc, tt.newSize, volumeSpec)
+			require.NoError(t, err)
+
+			updatedSize := volumeSpec.PersistentVolumeClaim.Resources.Requests[corev1.ResourceStorage]
+			assert.Equal(t, tt.newSize.Value(), updatedSize.Value())
+			assert.NotEqual(t, originalSize.Value(), updatedSize.Value())
+			assert.NotNil(t, tt.cr.Status.StorageAutoscaling)
+
+			status, exists := tt.cr.Status.StorageAutoscaling[tt.pvc.Name]
+			assert.True(t, exists)
+			assert.Equal(t, tt.expectedResize, status.ResizeCount)
+			assert.False(t, status.LastResizeTime.IsZero())
 		})
 	}
 }
