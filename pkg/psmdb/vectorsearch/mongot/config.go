a326f03b85add4e679a66ded5d78e41c9e15616a
Ege Güneş <ege.gunes@percona.com>
2026-07-06 14:27:08 +0300

    K8SPSMDB-1654: Vector search fixes/improvements
    - Mongot statefulset shouldn't be created for config server replset.
    - Update mongot configuration to support changes in upstream.
    - Use mongot image provided by Percona.

--- pkg/psmdb/vectorsearch/mongot/config.go
+++ pkg/psmdb/vectorsearch/mongot/config.go
@@ -18,33 +18,51 @@ type EmbeddingConfig struct {
 }
 
 type ConfigSyncSource struct {
-	ReplicaSet               ConfigReplicaSet `json:"replicaSet" yaml:"replicaSet"`
-	Router                   *ConfigRouter    `json:"router,omitempty" yaml:"router,omitempty"`
-	CertificateAuthorityFile *string          `json:"caFile,omitempty" yaml:"caFile,omitempty"`
+	ReplicaSet        ConfigReplicaSet         `json:"replicaSet" yaml:"replicaSet"`
+	Router            *ConfigRouter            `json:"router,omitempty" yaml:"router,omitempty"`
+	ReplicationReader *ConfigReplicationReader `json:"replicationReader,omitempty" yaml:"replicationReader,omitempty"`
 }
 
-type ConfigRouter struct {
-	HostAndPort  []string    `json:"hostAndPort" yaml:"hostAndPort"`
-	Username     string      `json:"username,omitempty" yaml:"username,omitempty"`
-	PasswordFile string      `json:"passwordFile,omitempty" yaml:"passwordFile,omitempty"`
-	TLS          *bool       `json:"tls,omitempty" yaml:"tls,omitempty"`
-	AuthSource   *string     `json:"authSource,omitempty" yaml:"authSource,omitempty"`
-	X509         *ConfigX509 `json:"x509,omitempty" yaml:"x509,omitempty"`
+type ConfigReplicationReader struct {
+	ReadPreference *string       `json:"readPreference,omitempty" yaml:"readPreference,omitempty"`
+	TagSets        [][]ConfigTag `json:"tagSets,omitempty" yaml:"tagSets,omitempty"`
 }
 
-type ConfigReplicaSet struct {
-	HostAndPort    []string    `json:"hostAndPort" yaml:"hostAndPort"`
-	Username       string      `json:"username,omitempty" yaml:"username,omitempty"`
-	PasswordFile   string      `json:"passwordFile,omitempty" yaml:"passwordFile,omitempty"`
-	TLS            *bool       `json:"tls,omitempty" yaml:"tls,omitempty"`
-	ReadPreference *string     `json:"readPreference,omitempty" yaml:"readPreference,omitempty"`
-	AuthSource     *string     `json:"authSource,omitempty" yaml:"authSource,omitempty"`
-	X509           *ConfigX509 `json:"x509,omitempty" yaml:"x509,omitempty"`
+type ConfigTag struct {
+	Name  string `json:"name" yaml:"name,omitempty"`
+	Value string `json:"value" yaml:"value,omitempty"`
+}
+
+type ScramAuthTLS struct {
+	Enabled                           bool    `json:"enabled" yaml:"enabled,omitempty"`
+	TLSCertificateKeyFile             *string `json:"tlsCertificateKeyFile,omitempty" yaml:"tlsCertificateKeyFile,omitempty"`
+	TLSCertificateKeyFilePasswordFile *string `json:"tlsCertificateKeyFilePasswordFile,omitempty" yaml:"tlsCertificateKeyFilePasswordFile,omitempty"`
+	CertificateAuthorityFile          *string `json:"caFile,omitempty" yaml:"caFile,omitempty"`
+}
+
+type ConfigScramAuth struct {
+	Username     string        `json:"username" yaml:"username,omitempty"`
+	PasswordFile string        `json:"passwordFile" yaml:"passwordFile,omitempty"`
+	TLS          *ScramAuthTLS `json:"tls,omitempty" yaml:"tls,omitempty"`
+	AuthSource   *string       `json:"authSource,omitempty" yaml:"authSource,omitempty"`
 }
 
 type ConfigX509 struct {
+	CertificateAuthorityFile          *string `json:"caFile,omitempty" yaml:"caFile,omitempty"`
 	TLSCertificateKeyFile             *string `json:"tlsCertificateKeyFile,omitempty" yaml:"tlsCertificateKeyFile,omitempty"`
 	TLSCertificateKeyFilePasswordFile *string `json:"tlsCertificateKeyFilePasswordFile,omitempty" yaml:"tlsCertificateKeyFilePasswordFile,omitempty"`
+}
+
+type ConfigRouter struct {
+	HostAndPort []string         `json:"hostAndPort" yaml:"hostAndPort"`
+	X509        *ConfigX509      `json:"x509,omitempty" yaml:"x509,omitempty"`
+	ScramAuth   *ConfigScramAuth `json:"scramAuth,omitempty" yaml:"scramAuth,omitempty"`
+}
+
+type ConfigReplicaSet struct {
+	HostAndPort []string         `json:"hostAndPort" yaml:"hostAndPort"`
+	X509        *ConfigX509      `json:"x509,omitempty" yaml:"x509,omitempty"`
+	ScramAuth   *ConfigScramAuth `json:"scramAuth,omitempty" yaml:"scramAuth,omitempty"`
 }
 
 type ConfigStorage struct {
