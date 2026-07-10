684ef2660db9984aa1de4aa7b630c6e3788dde7b
George Kechagias <george.kechagias@percona.com>
2026-07-10 20:00:23 +0300

    remove unused import

--- pkg/controller/perconaservermongodbclustersync/psmdb_clustersync_controller_test.go
+++ pkg/controller/perconaservermongodbclustersync/psmdb_clustersync_controller_test.go
@@ -13,7 +13,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
